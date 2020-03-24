9bb08b3c7387de446c78b4554ad9b395f5ff36ec
Jeffrey A. ZAMORA <zamoraja@ph.ibm.com>
2020-03-24 13:32:16 +0800

    update checker class

---
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/UpdateChecker.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.ibm.cio.cmr.request.automation.util;
+
+import com.ibm.cio.cmr.request.automation.impl.gbl.UpdateSwitchElement;
+
+/**
+ * Checks the updates being done on a request and is called from the
+ * {@link UpdateSwitchElement}
+ * 
+ * @author JeffZAMORA
+ * 
+ */
+public abstract class UpdateChecker {
+
+}
