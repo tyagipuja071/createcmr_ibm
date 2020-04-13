0ff4f3d5c42b1c58306116ab15718d72a24359ea
Chi Xu <dlxchi@cn.ibm.com>
2020-04-13 16:25:36 +0800

    CMR-2191 RESTORE

--- RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
@@ -6464,27 +6464,25 @@ function toggleBPRelMemType() {
   }
 }
 
-// function toggleBPRelMemTypeForTurkey(){
-// var reqType = null;
-// // var role = null;
-// if (typeof (_pagemodel) != 'undefined') {
-// reqType = FormManager.getActualValue('reqType');
-// // role = _pagemodel.userRole.toUpperCase();
-// }
-// if (FormManager.getActualValue('viewOnlyPage') == 'true') {
-// return;
-// }
-// if(reqType == 'U'){
-// FormManager.show('MembLevel', 'memLvl');
-// FormManager.show('BPRelationType', 'bpRelType');
-// FormManager.resetValidations('bpRelType');
-// FormManager.resetValidations('memLvl');
-// FormManager.addValidator('memLvl', Validators.REQUIRED, [ 'Membership Level'
-// ], 'MAIN_IBM_TAB');
-// FormManager.addValidator('bpRelType', Validators.REQUIRED, [ 'BP Relation
-// Type' ], 'MAIN_IBM_TAB');
-// }
-// }
+ function toggleBPRelMemTypeForTurkey(){
+	 var reqType = null;
+	 // var role = null;
+	 if (typeof (_pagemodel) != 'undefined') {
+	 reqType = FormManager.getActualValue('reqType');
+	 // role = _pagemodel.userRole.toUpperCase();
+	 }
+	 if (FormManager.getActualValue('viewOnlyPage') == 'true') {
+	 return;
+	 }
+	 if(reqType == 'U'){
+	 FormManager.show('MembLevel', 'memLvl');
+	 FormManager.show('BPRelationType', 'bpRelType');
+	 FormManager.resetValidations('bpRelType');
+	 FormManager.resetValidations('memLvl');
+	 FormManager.addValidator('memLvl', Validators.REQUIRED, [ 'Membership Level' ], 'MAIN_IBM_TAB');
+	 FormManager.addValidator('bpRelType', Validators.REQUIRED, [ 'BP Relation Type' ], 'MAIN_IBM_TAB');
+	 }
+ }
 
 function unlockINACForINTERUKI() {
   var issu_cntry = FormManager.getActualValue('cmrIssuingCntry');
@@ -7394,7 +7392,7 @@ dojo.addOnLoad(function() {
    GEOHandler.addAfterConfig(setSBOLogicOnISUChange, [ SysLoc.TURKEY ]);
   // CMR-2574 ISU+CTC
   GEOHandler.addAfterTemplateLoad(setISUCTCBasedScenarios, [ SysLoc.TURKEY ]);
-  // GEOHandler.addAfterConfig(toggleBPRelMemTypeForTurkey, [ SysLoc.TURKEY ]);
+  GEOHandler.addAfterConfig(toggleBPRelMemTypeForTurkey, [ SysLoc.TURKEY ]);
 
   // Greece
   GEOHandler.addAfterConfig(addHandlersForGRCYTR, [ SysLoc.GREECE, SysLoc.CYPRUS, SysLoc.TURKEY ]);
