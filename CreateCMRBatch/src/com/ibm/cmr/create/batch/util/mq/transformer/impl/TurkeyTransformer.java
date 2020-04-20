9bc388deec24e11a3b07a6e5abcc7becf5d6ceef
JUN HE XIAO <xiaojhe@cn.ibm.com>
2020-04-20 16:32:52 +0800

    CMR-2398_Address_change

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/TurkeyTransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/TurkeyTransformer.java
@@ -1658,7 +1658,7 @@ public class TurkeyTransformer extends EMEATransformer {
 
   private void copyMailingFromBilling(LegacyDirectObjectContainer legacyObjects, CmrtAddr billingAddr) {
     CmrtAddr mailingAddr = (CmrtAddr) SerializationUtils.clone(billingAddr);
-    mailingAddr.getId().setAddrNo("00001");
+    mailingAddr.getId().setAddrNo("00002");
     mailingAddr.setIsAddrUseMailing(ADDRESS_USE_EXISTS);
     // modifyAddrUseFields(MQMsgConstants.SOF_ADDRESS_USE_MAILING, mailingAddr);
     legacyObjects.getAddresses().add(mailingAddr);
