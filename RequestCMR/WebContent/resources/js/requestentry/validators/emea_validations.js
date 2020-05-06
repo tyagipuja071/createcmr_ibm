09a87fd65c55a2755c4f8013d113af2d7a978189
Chi Xu <dlxchi@cn.ibm.com>
2020-05-06 11:55:29 +0800

    changed values to [0]

--- RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
@@ -3242,7 +3242,7 @@ function addTRAddressTypeValidator() {
           for (var i = 0; i < compareFields.length; i++) {
             var custType = FormManager.getActualValue('custGrp');
             if (custType == 'CROSS') {
-              if (zs01Copy[compareFields[i]].values != zp01Copy[compareFields[i]].values) {
+              if (zs01Copy[compareFields[i]][0] != zp01Copy[compareFields[i]][0]) {
                 return new ValidationResult(null, false, 'Local language not applicable for Cross-border, address must match sold to data.');
               }
             } else {
