bc731b9bcfa86b3115c3956653b678457c5169e8
Anuja Srivastava <anujsr27@gmail.com>
2020-05-21 18:01:53 +0530

    CMR-3757:PPSCEID_mandatory_for_BP_scenario

--- RequestCMR/WebContent/resources/js/requestentry/validators/us_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/us_validations.js
@@ -106,6 +106,18 @@ function addAddressRecordTypeValidator() {
  * After configuration for US
  */
 function afterConfigForUS() {
+  // PPS CEID field as mandatory for BP scenario
+  var custTypeHandler = null;
+  if (custTypeHandler == null) {
+    var custTypeHandler = dojo.connect(FormManager.getField('custType'), 'onChange', function(value) {
+      if (FormManager.getActualValue('userRole').toUpperCase() == 'REQUESTER' && FormManager.getActualValue('reqType') == 'C' && FormManager.getActualValue('custType') == '7') {
+        FormManager.addValidator('ppsceid', Validators.REQUIRED, [ 'PPSCEID' ], 'MAIN_IBM_TAB');
+      } else {
+        FormManager.removeValidator('ppsceid', Validators.REQUIRED);
+      }
+    });
+  }
+
   var usCntryHandler = null;
   if (usCntryHandler == null) {
     // set the postal code to 0000 for non-US landed countries and disable
