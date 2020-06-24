d2a3dc7a89a4a6f5a4db0a511bc06088d3f135c4
ganarang <ganarang@in.ibm.com>
2020-06-24 19:58:35 +0530

    onboarding util

---
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/legacy/LegacyCommonUtil.java
@@ -0,0 +1,15 @@
+package com.ibm.cio.cmr.request.util.legacy;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Class that contains utilities for Legacy
+ *
+ * @author JeffZAMORA
+ *
+ */
+public class LegacyCommonUtil {
+
+  private static final Logger LOG = Logger.getLogger(LegacyCommonUtil.class);
+
+}
