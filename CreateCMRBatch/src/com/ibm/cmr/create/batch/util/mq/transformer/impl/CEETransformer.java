9fe2ff4556f3e666fafd96771824e3f52ede2c10
HaoLiu <dllhao@cn.ibm.com>
2020-07-20 10:54:34 +0800

    CEE CMKDA field for bp scenario

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/CEETransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/CEETransformer.java
@@ -1198,7 +1198,7 @@ public class CEETransformer extends EMEATransformer {
     // data.getIbmDeptCostCenter() : "");
     if (StringUtils.isEmpty(data.getCrosSubTyp())) {
       legacyCust.setMrcCd("3");
-    } else if (!StringUtils.isEmpty(data.getCrosSubTyp()) && ("BP".equals(data.getCrosSubTyp()) || "XBP".equals(data.getCrosSubTyp()))) {
+    } else if (!StringUtils.isEmpty(data.getCrosSubTyp()) && ("BP".equals(data.getCustSubGrp()) || "XBP".equals(data.getCustSubGrp()))) {
       legacyCust.setMrcCd("5");
     } else {
       legacyCust.setMrcCd("3");
