cf6b4b537c242d5741da53e88f9709cf77b336c5
HaoLiu <dllhao@cn.ibm.com>
2020-07-21 16:24:57 +0800

    CEE address map

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/CEETransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/CEETransformer.java
@@ -628,6 +628,12 @@ public class CEETransformer extends EMEATransformer {
       } else {
         line6 = "";
       }
+    } else {
+      if (!StringUtils.isBlank(addrData.getLandCntry())) {
+        line6 = LandedCountryMap.getCountryName(addrData.getLandCntry());
+      } else {
+        line6 = "";
+      }
     }
     // if (!StringUtils.isBlank(addrData.getLandCntry())) {
     // line6 = LandedCountryMap.getCountryName(addrData.getLandCntry());
@@ -652,7 +658,7 @@ public class CEETransformer extends EMEATransformer {
     legacyAddr.setAddrLine1(line1);
     legacyAddr.setAddrLine2(line2);
     legacyAddr.setAddrLine3(line3);
-//    legacyAddr.setAddrLine4(line4);
+    legacyAddr.setAddrLine4(line4);
     legacyAddr.setAddrLine5(line5);
     legacyAddr.setCity(addrData.getCity1());
     legacyAddr.setZipCode(addrData.getPostCd());
