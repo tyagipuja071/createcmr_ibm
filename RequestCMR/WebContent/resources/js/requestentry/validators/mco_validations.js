5b4c7093eaf04107546d2b5cef3ff5a0304df48b
Shivani Chauhan <schauh97@in.ibm.com>
2020-08-05 14:51:34 +0530

    CMR-5704- AbbrevName mandatory check removed for Update

--- RequestCMR/WebContent/resources/js/requestentry/validators/mco_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/mco_validations.js
@@ -954,7 +954,7 @@ function abbrvNmProcessorMandatory() {
     var viewOnlyPage = FormManager.getActualValue('viewOnlyPage');
 
     if (viewOnlyPage != 'true') {
-      if (role != 'REQUESTER') {
+      if (role != 'REQUESTER' && FormManager.getActualValue('reqType') != 'U') {
         FormManager.addValidator('abbrevNm', Validators.REQUIRED, [ 'AbbrevName' ], 'MAIN_CUST_TAB');
       } else {
         FormManager.removeValidator('abbrevNm', Validators.REQUIRED);
@@ -970,7 +970,7 @@ function abbrvNmProcessorMandatory() {
 function abbrvNmProcessorMandatoryOnChange() {
   var role = FormManager.getActualValue('userRole').toUpperCase();
   dojo.connect(FormManager.getField('abbrevNm'), 'onChange', function(value) {
-    if (role != 'REQUESTER') {
+    if (role != 'REQUESTER' && FormManager.getActualValue('reqType') != 'U') {
       FormManager.addValidator('abbrevNm', Validators.REQUIRED, [ 'AbbrevName' ], 'MAIN_CUST_TAB');
     } else {
       if(FormManager.getActualValue('reqType') != 'U'){
