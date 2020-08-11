492962dd2b39a2c63cb0a303385b30ad2c66baf5
HaoLiu <dllhao@cn.ibm.com>
2020-08-11 10:36:38 +0800

    DIC to custExt

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/CEETransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/CEETransformer.java
@@ -1680,11 +1680,14 @@ public class CEETransformer extends EMEATransformer {
       CMRRequestContainer cmrObjects) {
 
     Data data = cmrObjects.getData();
-    // if (!StringUtils.isBlank(data.getTaxCd1())) {
-    // legacyCustExt.setBankAcctNo(data.getTaxCd1());
-    // } else {
-    // legacyCustExt.setBankAcctNo("");
-    // }
+    if (SystemLocation.SLOVAKIA.equals(data.getCmrIssuingCntry())) {
+      if (!StringUtils.isBlank(data.getTaxCd1())) {
+        legacyCustExt.setBankAcctNo(data.getTaxCd1());
+      } else {
+        legacyCustExt.setBankAcctNo("");
+      }
+    }
+
     Admin admin = cmrObjects.getAdmin();
     if ("C".equals(admin.getReqType()) && "644".equals(data.getCmrIssuingCntry()) && "LOCAL".equals(data.getCustGrp())
         && !"Y".equals(data.getVatExempt())) {
@@ -1740,14 +1743,14 @@ public class CEETransformer extends EMEATransformer {
         }
       }
     }
-    //
-    // if (!StringUtils.isBlank(muData.getEmail2())) {
-    // if ("@".equals(muData.getEmail2())) {
-    // custExt.setBankAcctNo("");
-    // } else {
-    // custExt.setBankAcctNo(muData.getEmail2());
-    // }
-    // }
+
+    if (!StringUtils.isBlank(muData.getEmail2())) {
+      if ("@".equals(muData.getEmail2())) {
+        custExt.setBankAcctNo("");
+      } else {
+        custExt.setBankAcctNo(muData.getEmail2());
+      }
+    }
 
     List<MassUpdtAddr> muAddrList = cmrObjects.getMassUpdateAddresses();
     MassUpdtAddr zp01Addr = new MassUpdtAddr();
