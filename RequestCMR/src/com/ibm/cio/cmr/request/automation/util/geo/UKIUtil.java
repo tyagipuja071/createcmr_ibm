d6b1faf39e84735051114ab9a44cced1616fad4c
Shivangi <Shivangi@DESKTOP-0Q12T77>
2020-09-04 10:57:38 +0530

    CMR-5063 AND CMR-6257

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/UKIUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/UKIUtil.java
@@ -112,7 +112,12 @@ public class UKIUtil extends AutomationUtil {
       }
       break;
     case SCENARIO_THIRD_PARTY:
-      if (customerName.toUpperCase().equals(customerNameZI01.toUpperCase()) && !customerNameZI01.toUpperCase().matches("^VR[0-9]{3}.+$")) {
+      if (customerName.toUpperCase().equals(customerNameZI01.toUpperCase())) {
+        details.append("Customer Names on installing and billing address should be different for Third Party Scenario").append("\n");
+        engineData.addRejectionComment("OTH", "Customer Names on installing and billing address should be different for Third Party Scenario", "",
+            "");
+        return false;
+      } else if (!customerNameZI01.toUpperCase().matches("^VR[0-9]{3}.+$")) {
         details.append("The request does not meet the criteria for Third Party Scenario.").append("\n");
         engineData.addRejectionComment("OTH", "The request does not meet the criteria for Third Party Scenario.", "", "");
         return false;
@@ -135,6 +140,7 @@ public class UKIUtil extends AutomationUtil {
       }
     }
     return true;
+
   }
 
   @Override
