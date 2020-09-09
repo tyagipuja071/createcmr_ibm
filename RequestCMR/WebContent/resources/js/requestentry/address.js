ab6256b324213556193741d4a503349062b6bd82
zhi guang sun <sunzg@cn.ibm.com>
2020-09-09 22:00:44 +0800

    CMR-2781-address.js

--- RequestCMR/WebContent/resources/js/requestentry/address.js
+++ RequestCMR/WebContent/resources/js/requestentry/address.js
@@ -1747,14 +1747,14 @@ function applyAddrChangesModal_onLoad() {
       if (useCntry && type.ret3 != cntry) {
         break;
       }
-      
-      if(SysLoc.GREECE == cntry && type.ret1 == 'ZP01') {
-    	  if(FormManager.getActualValue('custGrp') == 'LOCAL') {
-    		  continue;
-    	  } else if (FormManager.getActualValue('reqType') == 'U' && FormManager.getActualValue('landCntry') == 'GR') {
-    		  continue;
-    	  } else if (FormManager.getActualValue('custGrp') == 'CROSS' && FormManager.getActualValue('addrType') == 'ZS01') {
-    		  continue;
+
+      if (SysLoc.GREECE == cntry && type.ret1 == 'ZP01') {
+        if (FormManager.getActualValue('custGrp') == 'LOCAL') {
+          continue;
+        } else if (FormManager.getActualValue('reqType') == 'U' && FormManager.getActualValue('landCntry') == 'GR') {
+          continue;
+        } else if (FormManager.getActualValue('custGrp') == 'CROSS' && FormManager.getActualValue('addrType') == 'ZS01') {
+          continue;
         }
       }
 
@@ -1912,6 +1912,10 @@ function applyAddrChangesModal_onLoad() {
           if (reqType != 'C' && typeof (GEOHandler) != 'undefined' && !GEOHandler.canCopyAddressType(type.ret1) && !single) {
             choices += '<input type="checkbox" name="copyTypes" value ="' + type.ret1 + '"><label class="cmr-radio-check-label">' + type.ret2 + ' (create additional only)</label><br>';
           } else if (cmr.currentAddressType && type.ret1 != cmr.currentAddressType) {
+            if (reqType == 'C'
+                && (type.ret1 == 'ZP02' || type.ret1 == 'ZP03' || type.ret1 == 'ZP04' || type.ret1 == 'ZP05' || type.ret1 == 'ZP06' || type.ret1 == 'ZP07' || type.ret1 == 'ZP08' || type.ret1 == 'ZI03')) {
+              continue;
+            }
             choices += '<input type="checkbox" name="copyTypes" value ="' + type.ret1 + '"><label class="cmr-radio-check-label">' + type.ret2 + '</label><br>';
           } else {
             choices += '<input type="checkbox" name="copyTypes" value ="' + type.ret1 + '"><label class="cmr-radio-check-label">' + type.ret2 + ' (copy only if others exist)</label><br>';
