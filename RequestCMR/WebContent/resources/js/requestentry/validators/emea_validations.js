3668abe52c44b7c4be11e358d2ee9a5d056fb9c5
potyagi1 <43468064+potyagi1@users.noreply.github.com>
2020-09-13 16:38:20 +0530

    INFSL scenario missing from autosetsbo logic -> added

--- RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
@@ -902,14 +902,14 @@ function autoSetAbbrevLocnOnAddSaveUKI(cntry, addressMode, saving, finalSave, fo
  * FormManager.setValue('abbrevLocn', _abbrevLocn); }
  */
 
-function autoSetVAT(){
-  
+function autoSetVAT() {
+
   if (PageManager.isReadOnly()) {
     return;
   }
-  
+
   var _custType = FormManager.getActualValue('custSubGrp');
-  if(!_custType || _custType == null){
+  if (!_custType || _custType == null) {
     _custType = '';
   }
   console.log(">>> Process autoSetVAT ...>> " + _custType);
@@ -999,7 +999,7 @@ function autoSetSBO(value, valueInDB) {
     if (custSubGrp == '') {
       return;
     } else if (custSubGrp == 'COMME' || custSubGrp == 'IGF' || custSubGrp == 'XIGF' || custSubGrp == 'COMLC' || custSubGrp == 'COOEM' || custSubGrp == 'SOFTL' || custSubGrp == 'THDPT'
-        || custSubGrp == 'CROSS' || custSubGrp == 'XGOVR') {
+        || custSubGrp == 'CROSS' || custSubGrp == 'XGOVR' || custSubGrp == 'INFSL') {
       // FormManager.enable('salesBusOffCd');
       // FormManager.enable('repTeamMemberNo');
       FormManager.resetDropdownValues(FormManager.getField('salesBusOffCd'));
