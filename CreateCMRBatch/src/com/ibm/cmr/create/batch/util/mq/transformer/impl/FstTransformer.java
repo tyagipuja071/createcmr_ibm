811695ba158b6aa9de0e301a0133f3dad2e6b544
Cheyenne Frey Lazo <lazoc@ph.ibm.com>
2020-09-17 18:37:07 +0800

    CMR-4201 Africa COD and COF changes

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/FstTransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/FstTransformer.java
@@ -9,11 +9,16 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.ibm.cio.cmr.request.CmrConstants;
+import com.ibm.cio.cmr.request.config.SystemConfiguration;
 import com.ibm.cio.cmr.request.entity.Addr;
 import com.ibm.cio.cmr.request.entity.Admin;
 import com.ibm.cio.cmr.request.entity.CmrtAddr;
 import com.ibm.cio.cmr.request.entity.CmrtCust;
 import com.ibm.cio.cmr.request.entity.Data;
+import com.ibm.cio.cmr.request.entity.DataRdc;
+import com.ibm.cio.cmr.request.model.BatchEmailModel;
+import com.ibm.cio.cmr.request.util.SystemParameters;
+import com.ibm.cio.cmr.request.util.legacy.LegacyCommonUtil;
 import com.ibm.cmr.create.batch.util.CMRRequestContainer;
 import com.ibm.cmr.create.batch.util.mq.LandedCountryMap;
 import com.ibm.cmr.create.batch.util.mq.MQMsgConstants;
@@ -53,6 +58,17 @@ public class FstTransformer extends MCOTransformer {
       }
     } else if (CmrConstants.REQ_TYPE_UPDATE.equals(admin.getReqType())) {
       legacyCust.setCustType(data.getCrosSubTyp());
+
+      if (StringUtils.isNotBlank(data.getCommercialFinanced())) {
+        legacyCust.setModeOfPayment(data.getCommercialFinanced());
+      } else {
+        String cod = data.getCreditCd();
+        if ("Y".equals(cod)) {
+          legacyCust.setModeOfPayment("5");
+        } else if ("N".equals(cod)) {
+          legacyCust.setModeOfPayment("");
+        }
+      }
     }
 
     legacyCust.setAccAdminBo("");
@@ -354,5 +370,71 @@ public class FstTransformer extends MCOTransformer {
       generateCMRNoObj.setMin(990000);
       generateCMRNoObj.setMax(999999);
     }
+  }
+
+  @Override
+  public String getMailSendingFlag(Data data, Admin admin, EntityManager entityManager) {
+    DataRdc oldData = LegacyCommonUtil.getOldData(entityManager, String.valueOf(data.getId().getReqId()));
+    String oldCOD = oldData.getCreditCd();
+    String oldCOF = oldData.getCommercialFinanced();
+
+    if (StringUtils.isNotBlank(oldCOF) && StringUtils.isBlank(data.getCommercialFinanced())) {
+      return "COF";
+    } else if (StringUtils.isNotBlank(oldCOD) && (StringUtils.isBlank(data.getCreditCd()) || "N".equals(data.getCreditCd()))) {
+      return "COD";
+    } else {
+      return "NA";
+    }
+  }
+
+  @Override
+  public String getEmailTemplateName(String type) {
+    if ("COD".equals(type)) {
+      return "cmr-email_cod.html";
+    } else {
+      return "cmr-email_cof.html";
+    }
+  }
+
+  @Override
+  public BatchEmailModel getMailFormatParams(EntityManager entityManager, CMRRequestContainer cmrObjects, String type) {
+    BatchEmailModel params = new BatchEmailModel();
+    Data data = cmrObjects.getData();
+    Admin admin = cmrObjects.getAdmin();
+    String directUrlLink = SystemConfiguration.getValue("APPLICATION_URL") + "/login?r=" + admin.getId().getReqId();
+    directUrlLink = "Click <a href=\"" + directUrlLink + "\">Here</a>";
+    params.setRequesterName(admin.getRequesterNm());
+    params.setRequesterId(admin.getRequesterId());
+    params.setRequestId(Long.toString(admin.getId().getReqId()));
+    String custNm = admin.getMainCustNm1() + (StringUtils.isNotBlank(admin.getMainCustNm2()) ? ("" + admin.getMainCustNm2()) : "");
+    params.setCustNm(custNm.trim());
+    params.setIssuingCountry(data.getCmrIssuingCntry());
+    params.setCmrNumber(data.getCmrNo());
+    params.setSubregion("N/A");
+    params.setDirectUrlLink(directUrlLink);
+    String receipent = "";
+    String mailSubject = "";
+    if ("COD".equals(type)) {
+      mailSubject = SystemParameters.getString("COD_MAIL_SUBJECT");
+      receipent = SystemParameters.getString("COD_MAIL_RECIEVER");
+    } else if ("COF".equals(type)) {
+      mailSubject = SystemParameters.getString("COF_MAIL_SUBJECT");
+      receipent = SystemParameters.getString("COF_MAIL_RECIEVER");
+    }
+    params.setReceipent(receipent);
+    params.setMailSubject(mailSubject);
+    params.setStringToReplace("xxxxxx");
+    params.setValToBeReplaceBy(data.getCmrNo());
+    return params;
+  }
+
+  @Override
+  public String getReqStatusForSendingMail(String mailFlag) {
+    if ("COD".equals(mailFlag)) {
+      return "PCO";
+    } else if ("COF".equals(mailFlag)) {
+      return "COM";
+    }
+    return null;
   }
 }
