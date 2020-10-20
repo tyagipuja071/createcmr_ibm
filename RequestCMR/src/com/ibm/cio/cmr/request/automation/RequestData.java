3609cb7c980ebcc45c4cfeb8eda63aa23ecc94c7
Jeffrey A. ZAMORA <zamoraja@ph.ibm.com>
2020-10-20 22:57:08 +0800

    fixed refresh

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/RequestData.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/RequestData.java
@@ -50,17 +50,23 @@ public class RequestData {
     AdminPK adminPk = new AdminPK();
     adminPk.setReqId(reqId);
     this.admin = entityManager.find(Admin.class, adminPk);
-    entityManager.refresh(this.admin);
+    if (this.admin != null) {
+      entityManager.refresh(this.admin);
+    }
 
     DataPK dataPk = new DataPK();
     dataPk.setReqId(reqId);
     this.data = entityManager.find(Data.class, dataPk);
-    entityManager.refresh(this.data);
+    if (this.data != null) {
+      entityManager.refresh(this.data);
+    }
 
     ScorecardPK scorecardPk = new ScorecardPK();
     scorecardPk.setReqId(reqId);
     this.scorecard = entityManager.find(Scorecard.class, scorecardPk);
-    entityManager.refresh(this.scorecard);
+    if (this.scorecard != null) {
+      entityManager.refresh(this.scorecard);
+    }
 
     switch (admin.getReqType()) {
     case "C":
