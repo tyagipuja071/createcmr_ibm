3c6388b4b7469d40dfe1577e1bcee22c95b9c2fe
DhananjayYadav <DhananjayYadav@9.85.82.139>
2020-10-20 13:07:32 +0530

    phone

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/SouthAfricaTransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/SouthAfricaTransformer.java
@@ -160,7 +160,7 @@ public class SouthAfricaTransformer extends MCOTransformer {
     LOG.debug("transformLegacyAddressData South Africa transformer...");
     formatAddressLines(dummyHandler);
 
-    if (DEFAULT_CLEAR_CHAR.equals(currAddr.getId().getAddrType())) {
+    if (DEFAULT_CLEAR_NUM.equals(currAddr.getId().getAddrType())) {
       legacyAddr.setAddrPhone("");
     } else if ("ZD01".equals(currAddr.getId().getAddrType())) {
       legacyAddr.setAddrPhone(currAddr.getCustPhone());
