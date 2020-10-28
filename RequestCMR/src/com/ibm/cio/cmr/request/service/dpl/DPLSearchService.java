5f9e14c6db50af7202ec598243f236bf013c44e5
Jeffrey A. ZAMORA <zamoraja@ph.ibm.com>
2020-10-28 18:27:24 +0800

    add assessment on pdf

--- RequestCMR/src/com/ibm/cio/cmr/request/service/dpl/DPLSearchService.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/service/dpl/DPLSearchService.java
@@ -26,6 +26,7 @@ import com.ibm.cio.cmr.request.config.SystemConfiguration;
 import com.ibm.cio.cmr.request.entity.Addr;
 import com.ibm.cio.cmr.request.entity.Data;
 import com.ibm.cio.cmr.request.entity.Scorecard;
+import com.ibm.cio.cmr.request.entity.ScorecardPK;
 import com.ibm.cio.cmr.request.model.ParamContainer;
 import com.ibm.cio.cmr.request.service.BaseSimpleService;
 import com.ibm.cio.cmr.request.service.requestentry.AttachmentService;
@@ -109,9 +110,28 @@ public class DPLSearchService extends BaseSimpleService<Object> {
     String companyName = extractMainCompanyName(reqData);
     List<DPLSearchResults> results = getPlainDPLSearchResults(entityManager, params);
 
+    int resultCount = 0;
+    for (DPLSearchResults result : results) {
+      if (result.getDeniedPartyRecords() != null) {
+        resultCount += result.getDeniedPartyRecords().size();
+      }
+    }
+    ScorecardPK scorecardPk = new ScorecardPK();
+    scorecardPk.setReqId(reqId);
+    Scorecard scorecard = entityManager.find(Scorecard.class, scorecardPk);
+    if (scorecard != null && resultCount == 0) {
+      LOG.debug("Auto assessinging DPL check results.");
+      scorecard.setDplAssessmentBy("CreateCMR");
+      scorecard.setDplAssessmentCmt("No actual results found during the search.");
+      scorecard.setDplAssessmentResult("N");
+      scorecard.setDplAssessmentDate(SystemUtil.getActualTimestamp());
+      entityManager.merge(scorecard);
+    }
+
     AttachmentService attachmentService = new AttachmentService();
     Timestamp ts = SystemUtil.getActualTimestamp();
     DPLSearchPDFConverter pdf = new DPLSearchPDFConverter(user.getIntranetId(), ts.getTime(), companyName, results);
+    pdf.setScorecard(scorecard);
 
     String type = "";
     try {
@@ -131,6 +151,7 @@ public class DPLSearchService extends BaseSimpleService<Object> {
     String fileName = prefix + formatter.format(ts) + ".pdf";
 
     LOG.debug("Attaching " + fileName + " to Request " + reqId);
+
     boolean success = true;
     try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
       pdf.exportToPdf(null, null, null, bos, null);
