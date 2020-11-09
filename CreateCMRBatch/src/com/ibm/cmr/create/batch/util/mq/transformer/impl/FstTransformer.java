aa11a5af83e5e98b7c10ff232354b8c84f6038b5
Rochelle M. SALAZAR <salazarm@ph.ibm.com>
2020-11-09 21:49:16 +0800

    mco2 fix db2 fields default value

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/FstTransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/FstTransformer.java
@@ -134,7 +134,7 @@ public class FstTransformer extends MCOTransformer {
     legacyCust.setMailingCond("");
     legacyCust.setAuthRemarketerInd("0");
 
-    if (MQMsgConstants.CUSTSUBGRP_BUSPR.equals(custType) || "XBP".equals(custType)) {
+    if (MQMsgConstants.CUSTSUBGRP_BUSPR.equals(custType) || "XBP".equals(custType) || "LLCBP".equals(custType)) {
       legacyCust.setMrcCd("5");
       legacyCust.setAuthRemarketerInd("1");
     } else {
