c5230b10bafcf71f610de5992762b8a025acdc63
DevangGhiya <devghiya@in.ibm.com>
2020-12-16 13:02:34 +0530

    CMR-201 Defect fix

--- RequestCMR/src/com/ibm/cio/cmr/request/util/legacy/LegacyDirectUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/legacy/LegacyDirectUtil.java
@@ -813,30 +813,6 @@ public class LegacyDirectUtil {
     return tempList;
   }
 
-  public static List<String> addSpecialCharToLovDR(List<String> lovList, String cntry, boolean codeOnly, String fieldId) {
-    List<String> tempList = new ArrayList<String>();
-    // if (SystemLocation.UNITED_KINGDOM.equals(cntry) ||
-    // SystemLocation.IRELAND.equals(cntry)) {
-    if (FIELDS_CLEAR_LIST.contains(fieldId)) {
-      LOG.debug("***Field " + fieldId + " is on clear list. Adding '@'");
-      if (codeOnly) {
-        tempList.add("@");
-      } /*
-         * else { tempList.add("@ | Clear field"); }
-         */
-      for (String choice : lovList) {
-        tempList.add(choice);
-      }
-    } else {
-      LOG.debug("***Field " + fieldId + " is NOT on clear list. Returning ORIGINAL list.");
-      tempList = lovList;
-    }
-    // } else {
-    // tempList = lovList;
-    // }
-    return tempList;
-  }
-
   private static String validateColValFromCell(XSSFCell cell) {
     String colVal = "";
     if (cell != null) {
