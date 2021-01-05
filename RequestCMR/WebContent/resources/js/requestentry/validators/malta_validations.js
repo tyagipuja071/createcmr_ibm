58157459bb52fefb07a8a3fec1993af71e4af36e
ganarang <ganarang@in.ibm.com>
2021-01-05 21:08:41 +0530

    com 638

--- RequestCMR/WebContent/resources/js/requestentry/validators/malta_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/malta_validations.js
@@ -89,14 +89,16 @@ function classFieldBehaviour() {
   }
 }
 
-function disableAddrFieldsMT() {
+function disableAddrFieldsMT(cntry, addressMode, saving, finalSave, force) {
   var custType = FormManager.getActualValue('custGrp');
   var addrType = FormManager.getActualValue('addrType');
 
   if (custType == 'LOCAL' && addrType == 'ZS01') {
     FormManager.readOnly('landCntry');
   } else {
-    FormManager.setValue('landCntry', FormManager.getActualValue('defaultLandedCountry'));
+    if (!saving && addressMode != 'updateAddress') {
+      FormManager.setValue('landCntry', FormManager.getActualValue('defaultLandedCountry'));
+    }
     FormManager.enable('landCntry');
   }
   // Phone - for shipping
