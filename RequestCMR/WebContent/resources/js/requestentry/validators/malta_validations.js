dce05ebce5e6fdfe4662c3191b0346d5f1c6300d
ganarang <ganarang@in.ibm.com>
2021-01-06 14:14:29 +0530

    com 646

--- RequestCMR/WebContent/resources/js/requestentry/validators/malta_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/malta_validations.js
@@ -762,7 +762,7 @@ function disableEnableFieldsForMT() {
     FormManager.readOnly('specialTaxCd');
     FormManager.readOnly('custPrefLang');
   } else if (reqType == 'C' && role == 'PROCESSOR') {
-    FormManager.readOnly('cmrNo');
+    FormManager.enable('cmrNo');
   } else {
     FormManager.enable('cmrNo');
     FormManager.enable('cmrOwner');
