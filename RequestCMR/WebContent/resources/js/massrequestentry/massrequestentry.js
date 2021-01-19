258f295dbc9a78e0d60429652267f8068684c757
JOYCEANNBARCELONA <JOYCEANNBARCELONA@9.102.40.168>
2021-01-19 21:05:23 +0800

    CREATCMR-243: Revert Company Proof attachment for Africa

--- RequestCMR/WebContent/resources/js/massrequestentry/massrequestentry.js
+++ RequestCMR/WebContent/resources/js/massrequestentry/massrequestentry.js
@@ -333,39 +333,6 @@ function isNewMassTemplateUsed() {
     }
 }
 
-function isCompanyProofNeeded() {
-  console.log('>> Executing isCompanyProofNeeded()');
-
-  var MCOAFRICA = [ '373', '382', '383', '610', '635', '636', '637', '645', '656', '662', '667', '669', '670', '691', '692', '698', '700', '717', '718', '725', '745', '753', '764', '769', '770',
-      '782', '804', '810', '825', '827', '831', '833', '835', '840', '841', '842', '851', '857', '876', '879', '880', '881', '883' ];
-  var cmrCntry = FormManager.getActualValue('cmrIssuingCntry');
-  var reqId = FormManager.getActualValue('procReqId');
-  var iterId = FormManager.getActualValue('iterId');
-
-  if (MCOAFRICA.includes(cmrCntry)) {
-    var qParams = {
-      ID : reqId,
-      ITER_ID : iterId,
-    };
-
-    var result = cmr.query('COUNT.LD_MASS_UPDT_ADDR_ZS01_ZP01', qParams);
-
-    if (result != null && result.ret1 > 0) {
-
-      var resultAttachment = cmr.query('CHECK_DNB_MATCH_ATTACHMENT', qParams);
-      var attachmentCount = resultAttachment.ret1;
-
-      if (attachmentCount > 0) {
-        return false;
-      }
-      return true;
-    } else {
-      return false;
-    }
-  }
-  return false;
-}
-
 /**
  * Validate function
  */
@@ -374,8 +341,6 @@ function doValidateRequest() {
     MessageMgr.showErrorMessage('DPL Check is needed for uploaded template.');
   } else if (isAttachmentNeeded() && _pagemodel.userRole.toUpperCase() == "PROCESSOR" && isNewMassTemplateUsed()) {
     MessageMgr.showErrorMessage('DPL Matching results has not been attached to the request. This is required since DPL checks failed for one or more addresses.');
-  } else if (isCompanyProofNeeded()) {
-    MessageMgr.showErrorMessage('Proof of address is mandatory. Please attach Company Proof.');
   } else if (FormManager.validate('frmCMR')) {
     MessageMgr.showInfoMessage('The request has no errors.', null, true);
   } else {
