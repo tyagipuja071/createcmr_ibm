501595f737fcdbe0785582b84b7bb872165f642d
DevangGhiya <devghiya@in.ibm.com>
2021-01-25 14:25:03 +0530

    CMR-572 issue fix

--- RequestCMR/WebContent/resources/js/requestentry/validators/italy_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/italy_validations.js
@@ -1558,11 +1558,13 @@ function addBillingAddrValidator() {
 function addCMRValidator(){
 	var role = FormManager.getActualValue('userRole').toUpperCase();
 	var custSubType = FormManager.getActualValue('custSubGrp');
-   if (FormManager.getActualValue('reqType') == 'C' && (FormManager.getActualValue('findCmrResult') == 'NOT DONE' || FormManager.getActualValue('findCmrResult') == 'REJECTED')) {
+   if (FormManager.getActualValue('reqType') == 'C'){
+	if(FormManager.getActualValue('findCmrResult') == 'NOT DONE' || FormManager.getActualValue('findCmrResult') == 'REJECTED')) {
       if (role == "REQUESTER" && (custSubType == '3PAIT' || custSubType == '3PASM' || custSubType == '3PAVA' || custSubType == 'CRO3P')) {
        return new ValidationResult(null, false,'For 3rd party scenario please import a CMR via CMR search');
         }
-      } 
+      }
+    } 
 }
 
 function setVATForItaly() {
