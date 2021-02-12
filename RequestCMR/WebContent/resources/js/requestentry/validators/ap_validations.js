516d02f0bc0c4f6319408fcc420ca3e81f8f467e
DhananjayYadav <DhananjayYadav@9.85.110.88>
2021-02-12 10:27:29 +0530

    ANZ ISIC+ISU Fix !!

--- RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
@@ -2850,6 +2850,7 @@ dojo.addOnLoad(function() {
   GEOHandler.addAfterConfig(onIsicChange, [ SysLoc.AUSTRALIA, SysLoc.SINGAPORE ]);
   GEOHandler.addAfterTemplateLoad(onIsicChange, [ SysLoc.AUSTRALIA, SysLoc.SINGAPORE ]);
   
+  GEOHandler.addAfterConfig(onIsicChangeHandlerAP, GEOHandler.ANZ);
   GEOHandler.addAfterConfig(onIsicChangeHandlerAP, GEOHandler.ASEAN);
 
 });
