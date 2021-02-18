d247e112c151a4ad9947c764ae8a84a963e02278
Shivani Chauhan <schauh97@in.ibm.com>
2021-02-18 12:45:04 +0530

    Merge error fix

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/ASEANTransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/ASEANTransformer.java
@@ -53,15 +53,13 @@ public abstract class ASEANTransformer extends APTransformer {
       handler.messageHash.put("MrktRespCode", "2");
     setAbbLoc(handler);
 
-<<<<<<< HEAD
     String clusterID = handler.cmrData.getApCustClusterId();
     if (clusterID.contains("BLAN")) {
       handler.messageHash.put("ClusterNo", "");
     } else {
       handler.messageHash.put("ClusterNo", clusterID);
     }
-=======
->>>>>>> common_code
+
   }
 
   @Override
