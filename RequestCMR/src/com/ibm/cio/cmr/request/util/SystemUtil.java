831feef734214baf9a3502d6bda6de160f76cfb2
potyagi1 <43468064+potyagi1@users.noreply.github.com>
2021-02-23 17:42:07 +0530

    fix

--- RequestCMR/src/com/ibm/cio/cmr/request/util/SystemUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/SystemUtil.java
@@ -159,7 +159,7 @@ public class SystemUtil {
     return getCurrentTimestamp();
   }
 
-  public static synchronized long getNextID(EntityManager entityManager, String mandt, String idType) throws CmrException, SQLException {
+  public static long getNextID(EntityManager entityManager, String mandt, String idType) throws CmrException, SQLException {
     return getNextID(entityManager, mandt, idType, "CREQCMR");
   }
 
@@ -171,7 +171,8 @@ public class SystemUtil {
    * @throws CmrException
    * @throws SQLException
    */
-  public static long getNextID(EntityManager entityManager, String mandt, String idType, String schema) throws CmrException, SQLException {
+  public static synchronized long getNextID(EntityManager entityManager, String mandt, String idType, String schema)
+      throws CmrException, SQLException {
 
     String schemaToUse = schema != null ? schema + "." : "";
     String sql = "select NEXT VALUE for " + schemaToUse + "SEQ_" + idType + ", '' from SYSIBM.SYSDUMMY1";
