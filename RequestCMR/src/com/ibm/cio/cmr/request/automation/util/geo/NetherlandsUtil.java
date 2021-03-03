8ddf12980a801481617f28f56e71ba76e21199e2
potyagi1 <43468064+potyagi1@users.noreply.github.com>
2021-03-03 12:12:43 +0530

    netherlands util added

---
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/NetherlandsUtil.java
@@ -0,0 +1,39 @@
+package com.ibm.cio.cmr.request.automation.util.geo;
+
+import javax.persistence.EntityManager;
+
+import org.apache.log4j.Logger;
+
+import com.ibm.cio.cmr.request.automation.AutomationEngineData;
+import com.ibm.cio.cmr.request.automation.RequestData;
+import com.ibm.cio.cmr.request.automation.impl.gbl.CalculateCoverageElement;
+import com.ibm.cio.cmr.request.automation.out.AutomationResult;
+import com.ibm.cio.cmr.request.automation.out.OverrideOutput;
+import com.ibm.cio.cmr.request.automation.out.ValidationOutput;
+import com.ibm.cio.cmr.request.automation.util.AutomationUtil;
+import com.ibm.cio.cmr.request.automation.util.CoverageContainer;
+
+public class NetherlandsUtil extends AutomationUtil {
+
+  private static final Logger LOG = Logger.getLogger(NetherlandsUtil.class);
+
+  @Override
+  public boolean performScenarioValidation(EntityManager entityManager, RequestData requestData, AutomationEngineData engineData,
+      AutomationResult<ValidationOutput> result, StringBuilder details, ValidationOutput output) {
+    return true;
+  }
+
+  @Override
+  public boolean performCountrySpecificCoverageCalculations(CalculateCoverageElement covElement, EntityManager entityManager,
+      AutomationResult<OverrideOutput> results, StringBuilder details, OverrideOutput overrides, RequestData requestData,
+      AutomationEngineData engineData, String covFrom, CoverageContainer container, boolean isCoverageCalculated) throws Exception {
+    return true;
+  }
+
+  @Override
+  public AutomationResult<OverrideOutput> doCountryFieldComputations(EntityManager entityManager, AutomationResult<OverrideOutput> results,
+      StringBuilder details, OverrideOutput overrides, RequestData requestData, AutomationEngineData engineData) throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
