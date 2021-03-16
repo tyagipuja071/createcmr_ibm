b72c0dd1079f98aca76d2cc6c2dab6b0d21bb354
RupansiRastogi <RupansiRastogi@9.195.39.21>
2021-03-16 19:26:27 +0530

    fix

--- RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/emea_validations.js
@@ -8557,6 +8557,7 @@ function autoSetAbbrNameUKI() {
   var reqId = FormManager.getActualValue('reqId');
   var cmrCntry = FormManager.getActualValue('cmrIssuingCntry');
   var abbName = FormManager.getActualValue('abbrevNm');
+  var role = FormManager.getActualValue('userRole').toUpperCase();
   // CMR - 5063
   if (custSubGrp == 'THDPT') {
     var result = cmr.query('GET.CUSTNM1_ADDR_UKI', {
@@ -8600,11 +8601,17 @@ function autoSetAbbrNameUKI() {
     // Defect-6793
     autoSetAbbrevNmFrmDept();
   } else {
-
-    var result = cmr.query('GET.CUSTNM1_ADDR_UKI', {
-      REQ_ID : reqId,
-      ADDR_TYPE : 'ZS01'
-    });
+    if (role == "PROCESSOR"){
+        var result = cmr.query('UKI.GET_ABBNAME_DATA', {
+  	       REQ_ID : reqId
+  	    });
+    } else if (role == "REQUESTER"){
+    	var result = cmr.query('GET.CUSTNM1_ADDR_UKI', {
+    	   REQ_ID : reqId,
+    	   ADDR_TYPE : 'ZS01'
+    	});
+    }
+    
     if (result.ret1 != undefined) {
       {
         var _abbrevNmValue = result.ret1 + (result.ret2 != undefined ? result.ret2 : '');
