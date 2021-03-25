abd8a0ea5723a009870806cf4b520aa0831fedf6
zhi guang sun <sunzg@cn.ibm.com>
2021-03-25 16:32:50 +0800

    createcmr 1844 perlanguage 2

--- RequestCMR/WebContent/resources/js/requestentry/validators/belux_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/belux_validations.js
@@ -79,6 +79,7 @@ function afterConfigForBELUX() {
 
   if ((custLang == null || custLang == '') && reqType == 'U') {
     FormManager.setValue('custPrefLang', 'N');
+
   }
   if (custGrp == 'CROSS' || custGrp == 'LUCRO') {
     FormManager.setValue('custPrefLang', 'E');
