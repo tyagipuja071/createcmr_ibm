3dafd6b80ae5e0ab3922060abd85252450d1e75d
221040672 <xiaojhe@cn.ibm.com>
2021-05-28 15:21:13 +0800

    update

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/NORDXHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/NORDXHandler.java
@@ -1476,7 +1476,7 @@ public class NORDXHandler extends BaseSOFHandler {
     if ("U".equals(admin.getReqType()) && "TREC".equals(admin.getReqReason())) {
       changeShareAddrFroTrce(entityManager, data.getId().getReqId(), SystemConfiguration.getValue("MANDT"), data.getCmrIssuingCntry(),
           data.getCmrNo());
-      updateChangeindToNForTrce(entityManager, data.getId().getReqId());
+      // updateChangeindToNForTrce(entityManager, data.getId().getReqId());
     }
   }
 
