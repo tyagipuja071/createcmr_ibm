63d98a33a632a349ce2bffc0809d8b474eef732c
221040672 <xiaojhe@cn.ibm.com>
2021-06-02 18:40:15 +0800

    update

--- RequestCMR/src/com/ibm/cio/cmr/request/util/pdf/impl/FRPDFConverter.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/pdf/impl/FRPDFConverter.java
@@ -4,16 +4,80 @@
 package com.ibm.cio.cmr.request.util.pdf.impl;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
 
 import javax.persistence.EntityManager;
 
+import org.apache.log4j.Logger;
+
+import com.ibm.cio.cmr.request.entity.Addr;
+import com.ibm.cio.cmr.request.entity.Admin;
 import com.ibm.cio.cmr.request.entity.Data;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Table;
 
 public class FRPDFConverter extends DefaultPDFConverter {
 
+  private static final Logger LOG = Logger.getLogger(DefaultPDFConverter.class);
+
   public FRPDFConverter(String cmrIssuingCntry) throws IOException {
     super(cmrIssuingCntry);
+  }
+
+  @Override
+  public boolean exportToPdf(EntityManager entityManager, Admin admin, Data data, OutputStream os, String sysLoc) throws IOException {
+
+    try {
+      PdfWriter writer = new PdfWriter(os);
+      try {
+        PdfDocument pdf = new PdfDocument(writer);
+        try {
+          Document document = new Document(pdf);
+          try {
+
+            document.add(createHeader("Details for Request ID " + admin.getId().getReqId()));
+
+            document.add(blankLine());
+            addMainDetails(admin, data, document);
+
+            if (Arrays.asList("CREATE", "UPDATE", "C", "U").contains(admin.getReqType().toUpperCase())) {
+              document.add(blankLine());
+              Addr soldTo = addAddressDetails(admin, data, entityManager, document);
+
+              document.add(blankLine());
+              addCustomerDetails(entityManager, data, document);
+
+              document.add(blankLine());
+              addIBMDetails(entityManager, data, document);
+
+              addExtraSections(entityManager, admin, data, sysLoc, soldTo, document);
+
+            }
+
+            if (Arrays.asList("REACTIVATE", "DELETE", "R", "D").contains(admin.getReqType().toUpperCase())) {
+              document.add(blankLine());
+              addDeleteReactivateDetails(admin, entityManager, document);
+            }
+
+          } finally {
+            document.close();
+          }
+        } finally {
+          if (!pdf.isClosed())
+            pdf.close();
+        }
+      } finally {
+        if (!writer.isCloseStream())
+          writer.close();
+      }
+      return true;
+    } catch (Exception e) {
+      LOG.error("Error in Generating PDF for Request ID " + admin.getId().getReqId(), e);
+      return false;
+    }
   }
 
   @Override
