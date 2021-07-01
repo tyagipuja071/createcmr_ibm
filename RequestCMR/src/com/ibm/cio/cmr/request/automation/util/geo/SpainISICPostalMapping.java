b03596ff1b83bb2973990a0be9932729e1af16b3
potyagi1 <43468064+potyagi1@users.noreply.github.com>
2021-07-01 18:45:46 +0530

    coverage story fo spain

---
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/SpainISICPostalMapping.java
@@ -0,0 +1,13 @@
+package com.ibm.cio.cmr.request.automation.util.geo;
+
+public class SpainISICPostalMapping {
+  private String isicCds;
+
+  public String getIsicCds() {
+    return isicCds;
+  }
+
+  public void setIsicCds(String isicCds) {
+    this.isicCds = isicCds;
+  }
+}
