574b784fff79a5d22c99b321053a6c4bd166d8cf
zhaohyz <zhaohyz@cn.ibm.com>
2021-08-11 11:16:18 +0800

    china2.0: CREATCMR-2460 remove searchTerm login in auto

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/ChinaUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/ChinaUtil.java
@@ -87,17 +87,22 @@ public class ChinaUtil extends AutomationUtil {
         result.setOnError(true);
       }
       break;
-    case SCENARIO_CROSS_CROSS:
-      if (!("00000".equals(data.getSearchTerm()) || "04182".equals(data.getSearchTerm()))) {
-        LOG.debug("Cluster allowed: Cluster=" + data.getSearchTerm() + " Scenario=" + data.getCustSubGrp());
-        result.setOnError(false);
-        details.append("Cluster allowed:Cluster=" + data.getSearchTerm() + " Scenario=" + data.getCustSubGrp() + " for the request.\n");
-      } else {
-        details.append("Cluster=" + data.getSearchTerm() + " should not be allowed for Scenario=" + data.getCustSubGrp() + " for the request.\n");
-        engineData.addRejectionComment("OTH", "Cluster=" + data.getSearchTerm() + " should not be allowed for this scenario", "", "");
-        result.setOnError(true);
-      }
-      break;
+    // case SCENARIO_CROSS_CROSS:
+    // if (!("00000".equals(data.getSearchTerm()) ||
+    // "04182".equals(data.getSearchTerm()))) {
+    // LOG.debug("Cluster allowed: Cluster=" + data.getSearchTerm() + "
+    // Scenario=" + data.getCustSubGrp());
+    // result.setOnError(false);
+    // details.append("Cluster allowed:Cluster=" + data.getSearchTerm() + "
+    // Scenario=" + data.getCustSubGrp() + " for the request.\n");
+    // } else {
+    // details.append("Cluster=" + data.getSearchTerm() + " should not be
+    // allowed for Scenario=" + data.getCustSubGrp() + " for the request.\n");
+    // engineData.addRejectionComment("OTH", "Cluster=" + data.getSearchTerm() +
+    // " should not be allowed for this scenario", "", "");
+    // result.setOnError(true);
+    // }
+    // break;
     case SCENARIO_LOCAL_AQSTN:
       if (!("00000".equals(data.getSearchTerm()) || "04182".equals(data.getSearchTerm()))) {
         LOG.debug("Cluster allowed: Cluster=" + data.getSearchTerm() + " Scenario=" + data.getCustSubGrp());
