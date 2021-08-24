464ab4099e5f3e2dfdae751d78e3606f8621e180
yssyudl <yssyudl@cn.ibm.com>
2021-08-24 17:23:44 +0800

    CREATCMR-3259: update bp scenario search term

--- RequestCMR/WebContent/resources/js/requestentry/validators/cn_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/cn_validations.js
@@ -221,15 +221,15 @@ function afterConfigForCN() {
     
     var ppsceidBP = FormManager.getActualValue('ppsceid');
     var searchTerm = FormManager.getActualValue('searchTerm');
-    var isuCd = FormManager.getActualValue('isuCd');
-    if (_pagemodel.userRole.toUpperCase() == 'REQUESTER' &&  ppsceidBP != undefined && ppsceidBP != null && ppsceidBP != '' && isuCd != undefined && isuCd != null && isuCd != '' && isuCd == '8B' && searchTerm != undefined && searchTerm != null && searchTerm != '' && searchTerm != '08036'){
+    var regString = /[a-zA-Z]+/;
+    if (_pagemodel.userRole.toUpperCase() == 'REQUESTER' &&  ppsceidBP != undefined && ppsceidBP != null && ppsceidBP != '' && searchTerm != undefined && searchTerm != null && searchTerm != '08036'){
       if (searchTerm == '04182') {
         FormManager.readOnly('searchTerm');
         FormManager.readOnly('isuCd');
         FormManager.readOnly('clientTier');
         FormManager.readOnly('inacType');
         FormManager.readOnly('inacCd');
-      } else {
+      } else if (searchTerm == '' || searchTerm == '00000' || regString.test(searchTerm)) {
         FormManager.setValue('searchTerm', '04182');
         FormManager.setValue('clientTier', 'Z');
         FormManager.readOnly('searchTerm');
