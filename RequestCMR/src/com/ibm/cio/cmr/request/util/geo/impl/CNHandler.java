841bfa93d68222b2f6981247800170cbb3946f6e
yssyudl <yssyudl@cn.ibm.com>
2021-08-25 14:12:19 +0800

    CREATCMR-3259: bp update search term

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/CNHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/CNHandler.java
@@ -734,7 +734,7 @@ public class CNHandler extends GEOHandler {
 
     if (!StringUtils.isBlank(admin.getReqType()) && admin.getReqType().equalsIgnoreCase("U") && !StringUtils.isBlank(data.getPpsceid())
         && !StringUtils.isBlank(admin.getReqStatus()) && admin.getReqStatus().equalsIgnoreCase("DRA")) {
-      if (data.getSearchTerm() == null || data.getSearchTerm().trim().equalsIgnoreCase("") || data.getSearchTerm().trim().equalsIgnoreCase("00000")
+      if (StringUtils.isBlank(data.getSearchTerm()) || data.getSearchTerm().trim().equalsIgnoreCase("00000")
           || data.getSearchTerm().matches("[a-zA-Z]")) {
         data.setClientTier("Z");
         data.setSearchTerm("04182");
