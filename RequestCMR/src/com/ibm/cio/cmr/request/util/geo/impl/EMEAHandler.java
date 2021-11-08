cfc3e91c670636cf0ea826d30bc40288e346c32a
Vaishali Varshney <vaishali.varshney@ibm.com>
2021-11-08 20:27:25 +0530

    fixed UKI

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/EMEAHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/EMEAHandler.java
@@ -244,7 +244,7 @@ public class EMEAHandler extends BaseSOFHandler {
             seqNo = record.getCmrAddrSeq();
             if (!StringUtils.isBlank(seqNo) && StringUtils.isNumeric(seqNo) && !("862".equals(cmrIssueCd) || "726".equals(cmrIssueCd))) {
 
-                sofUses = this.legacyObjects.getUsesBySequenceNo(seqNo);
+              sofUses = this.legacyObjects.getUsesBySequenceNo(seqNo);
               if (StringUtils.isNotBlank(record.getCmrAddrSeq()) && !sofUses.isEmpty()) {
                 for (String sofUse : sofUses) {
                   addrType = getAddressTypeByUse(sofUse);
@@ -284,7 +284,7 @@ public class EMEAHandler extends BaseSOFHandler {
                     converted.add(addr);
                   }
                 }
-              } else if (sofUses.isEmpty() && "ZP01".equals(record.getCmrAddrTypeCode()) &&StringUtils.isNotEmpty(record.getCmrOffice())) {
+              } else if (sofUses.isEmpty() && "ZP01".equals(record.getCmrAddrTypeCode()) && StringUtils.isNotEmpty(record.getExtWalletId())) {
                 record.setCmrAddrTypeCode("PG01");
                 addrType = record.getCmrAddrTypeCode();
                 if (!StringUtils.isEmpty(addrType)) {
