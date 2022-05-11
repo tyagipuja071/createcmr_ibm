605e02726e51d5545f274859b9881d67b90a9919
zhi hua gao <zhgao@cn.ibm.com>
2022-05-11 20:52:46 +0800

    at

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/service/ATMultiService.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/service/ATMultiService.java
@@ -1,20 +1,26 @@
 package com.ibm.cmr.create.batch.service;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 import javax.persistence.EntityManager;
 
-import org.apache.poi.ss.formula.functions.T;
-
+import com.ibm.cio.cmr.request.entity.Admin;
 import com.ibm.cio.cmr.request.entity.listeners.ChangeLogListener;
 
 /*
  * Multi-threaded service for {@link ATService}
  */
-public class ATMultiService extends MultiThreadedBatchService<T> {
+public class ATMultiService extends MultiThreadedBatchService<Admin> {
 
   private ATService service = new ATService();
+
+  public enum Mode {
+    Aborted, Normal, Mass
+  };
+
+  Mode mode = Mode.Normal;
 
   @Override
   public boolean isTransactional() {
@@ -22,16 +28,50 @@ public class ATMultiService extends MultiThreadedBatchService<T> {
   }
 
   @Override
-  public Boolean executeBatchForRequests(EntityManager entityManager, List<T> requests) throws Exception {
+  public Boolean executeBatchForRequests(EntityManager entityManager, List<Admin> requests) throws Exception {
     this.service.initClient();
     ChangeLogListener.setUser(BATCH_USER_ID);
 
-    return null;
+    switch (mode) {
+    case Aborted:
+      this.service.monitorAbortedRecords(entityManager);
+      break;
+    case Normal:
+      this.service.monitorCreqcmr(entityManager);
+      break;
+    case Mass:
+      this.service.monitorCreqcmrMassUpd(entityManager);
+      break;
+
+    default:
+      break;
+    }
+    keepAlive();
+    return true;
+
   }
 
   @Override
-  protected Queue<T> getRequestsToProcess(EntityManager entityManager) {
-    return null;
+  protected Queue<Admin> getRequestsToProcess(EntityManager entityManager) {
+    List<Admin> records = null;
+    switch (mode) {
+
+    case Normal:
+      records = this.service.getPendingRecords(entityManager);
+      break;
+    case Mass:
+      records = this.service.getPendingRecordsMassUpd(entityManager);
+      break;
+
+    default:
+      break;
+    }
+
+    Queue<Admin> queue = new LinkedList<>();
+    if (records != null && !records.isEmpty()) {
+      queue.addAll(records);
+    }
+    return queue;
   }
 
   @Override
