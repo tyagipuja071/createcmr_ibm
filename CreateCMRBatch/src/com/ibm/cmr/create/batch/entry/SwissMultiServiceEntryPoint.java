c8beac1b72ba24a113fbdd197af7c0b242fc43c1
Jeffrey A. ZAMORA <zamoraja@ph.ibm.com>
2022-05-13 15:07:16 +0800

    fixed init

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/entry/SwissMultiServiceEntryPoint.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/entry/SwissMultiServiceEntryPoint.java
@@ -14,12 +14,14 @@ import com.ibm.cmr.create.batch.service.SwissMultiService.Mode;
 public class SwissMultiServiceEntryPoint extends BatchEntryPoint {
 
   public static void main(String[] args) throws CmrException {
-    SwissMultiService service = new SwissMultiService();
+    SwissMultiService service = null;
     if (args != null && args.length > 0 && "MASS".equals(args[0])) {
       BatchEntryPoint.initContext("SwissMultiMass");
+      service = new SwissMultiService();
       service.setMode(Mode.MassUpdt);
     } else {
       BatchEntryPoint.initContext("SwissMulti");
+      service = new SwissMultiService();
       service.setMode(Mode.Single);
     }
     service.execute();
