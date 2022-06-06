49d4dc1e94d286b4d8d3de487da944af5d0220eb
Ayushi . <AYUSHI.Ayushi@ibm.com>
2022-06-06 15:16:23 +0530

    CREATCMR 4810

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/TurkeyHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/TurkeyHandler.java
@@ -4385,7 +4385,7 @@ public class TurkeyHandler extends BaseSOFHandler {
   }
 
   private void saveAddrCopyForTR(EntityManager entityManager, Addr addr, String addrType, String reqType) {
-    Addr addrCopy = SerializationUtils.clone(addr);
+    Addr addrCopy = (Addr) SerializationUtils.clone(addr);
     addrCopy.getId().setAddrType(addrType);
 
     if (addrType.equals("ZS01")) {
@@ -4406,7 +4406,7 @@ public class TurkeyHandler extends BaseSOFHandler {
 
   // START -- missing code greece code
   private void saveAddrCopyForGR(EntityManager entityManager, Addr addr, String addrType) {
-    Addr addrCopy = SerializationUtils.clone(addr);
+    Addr addrCopy = (Addr) SerializationUtils.clone(addr);
     addrCopy.getId().setAddrType(addrType);
 
     if (addrType.equals("ZP01")) {
