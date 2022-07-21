9e55d1b82ea9082ae9e8483aa63d9ab8d8a501ab
Tarishi Geetey <Tarishi.Geetey@ibm.com>
2022-07-21 17:03:06 +0530

    CREATCMR-4897_changes_prospect_mrc_sbo_not_mandatory

--- RequestCMR/WebContent/resources/js/requestentry/validators/la_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/la_validations.js
@@ -492,7 +492,9 @@ function afterConfigForLA() {
 
   if (dojo.byId('isuCd')) {
     // FormManager.disable('isuCd');
-    FormManager.readOnly('isuCd');
+    if(!checkForProspect()){
+      FormManager.readOnly('isuCd');
+    }
   }
   if (dojo.byId('collBoId' && _reqType != 'U')) {
     dojo.byId('collBoId').readOnly = true;
@@ -2646,22 +2648,30 @@ function setTaxRegimeMX() {
   }
 }
 
-//CREATCMR-4897  SBO and MRC to not be mandatory for Prospect conversion
+// CREATCMR-4897 SBO and MRC to not be mandatory for Prospect conversion
 function makeMrcSboOptionalForProspectLA() {
-	var ifProspect = FormManager.getActualValue('prospLegalInd');
-    if (dijit.byId('prospLegalInd')) {
-    	ifProspect = dijit.byId('prospLegalInd').get('checked') ? 'Y' : 'N';
-    }
+  ifProspect = checkForProspect();
     if('Y' == ifProspect){
     	if (typeof (_pagemodel) != 'undefined') {
     		if (_pagemodel.userRole.toUpperCase() == 'REQUESTER') {
-    			FormManager.resetValidations('mrcCd');
+    			      FormManager.resetValidations('mrcCd');
                 FormManager.resetValidations('salesBusOffCd');
-                FormManager.setValue("isuCd", "");
+                FormManager.setValue('isuCd', '');
+                FormManager.setValue('mrcCd', '');
+                FormManager.setValue('salesBusOffCd', '');
+                FormManager.enable('isuCd');
                 }
     		}
     	}
     console.log('SBO & MRC are non mandatory for Prospect');
+}
+
+function checkForProspect(){
+  var ifProspect = FormManager.getActualValue('prospLegalInd');
+  if (dijit.byId('prospLegalInd')) {
+    ifProspect = dijit.byId('prospLegalInd').get('checked') ? 'Y' : 'N';
+  }
+  return ifProspect;
 }
 
 /* Register LA Validators */
@@ -2745,7 +2755,7 @@ dojo.addOnLoad(function() {
   GEOHandler.addAfterTemplateLoad(setSortlForStateProvince, [ SysLoc.BRAZIL ]);
   GEOHandler.addAfterTemplateLoad(setTaxRegimeMX, [ SysLoc.MEXICO ]);
 
-  //CREATCMR-4897 SBO and MRC to not be mandatory for Prospect conversion
+  // CREATCMR-4897 SBO and MRC to not be mandatory for Prospect conversion
   GEOHandler.addAfterConfig(makeMrcSboOptionalForProspectLA, GEOHandler.LA);
   GEOHandler.addAfterTemplateLoad(makeMrcSboOptionalForProspectLA, GEOHandler.LA);
 });
