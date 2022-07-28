261b513765d1a15aaf1228d9361cd79b7206ab4d
potyagi1 <43468064+potyagi1@users.noreply.github.com>
2022-07-28 21:32:49 +0530

    CREATCMR-5722_BuildIssueFixSemiColonMisisng

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/SingaporeUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/SingaporeUtil.java
@@ -282,7 +282,7 @@ public class SingaporeUtil extends AutomationUtil {
       break;
     case SCENARIO_PRIVATE_CUSTOMER:
     case SCENARIO_CROSS_PRIVATE_CUSTOMER:
-      engineData.addPositiveCheckStatus(AutomationEngineData.SKIP_COVERAGE)
+      engineData.addPositiveCheckStatus(AutomationEngineData.SKIP_COVERAGE);
       return doPrivatePersonChecks(engineData, SystemLocation.SINGAPORE, soldTo.getLandCntry(), customerName, details, false, requestData);
     }
     result.setDetails(details.toString());
