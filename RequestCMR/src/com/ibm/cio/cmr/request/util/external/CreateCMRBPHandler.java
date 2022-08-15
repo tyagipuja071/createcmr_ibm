0c82884c06b0998651bf005770898e47edab35dd
Xiang Bin Liu <dlliuxb@cn.ibm.com>
2022-08-15 22:03:48 +0800

    CREATCMR-6274_BP_US_email_fix_main

--- RequestCMR/src/com/ibm/cio/cmr/request/util/external/CreateCMRBPHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/external/CreateCMRBPHandler.java
@@ -126,13 +126,13 @@ public class CreateCMRBPHandler implements ExternalSystemHandler {
       if (admin.getChildReqId() > 0) {
         Data theChindData = getChildData(entityManager, admin.getChildReqId());
         if (theChindData != null && StringUtils.isNotEmpty(theChindData.getCmrNo())) {
-          params.add(" BP CMR#: " + params.get(3)); // {16}
-          params.add(" IBM Direct CMR#: " + theChindData.getCmrNo()); // {17}
+        	params.add("<tr><th style=\"text-align:left\" width=\"200px\">BP CMR#:</th><td " + params.get(3) + "</td></tr>"); // {16}
+        	params.add("<tr><th style=\"text-align:left\" width=\"200px\">IBM Direct CMR#:</th><td " + theChindData.getCmrNo() + "</td></tr>"); // {17}
         } else {
           Data theData = getChildData(entityManager, admin.getId().getReqId());
           if (theData != null && StringUtils.isNotEmpty(theData.getCmrNo2())) {
-            params.add(" BP CMR#: " + params.get(3)); // {16}
-            params.add(" IBM Direct CMR#: " + theData.getCmrNo2()); // {17}
+            params.add("<tr><th style=\"text-align:left\" width=\"200px\">BP CMR#:</th><td " + params.get(3) + "</td></tr>"); // {16}
+        	params.add("<tr><th style=\"text-align:left\" width=\"200px\">IBM Direct CMR#:</th><td " + theData.getCmrNo() + "</td></tr>"); // {17}
           } else {
             params.add(""); // {16}
             params.add(""); // {17}
