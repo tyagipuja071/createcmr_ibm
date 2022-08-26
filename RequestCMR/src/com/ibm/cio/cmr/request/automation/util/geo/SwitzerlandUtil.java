a5e41ef508e1bebda390db327fe30051eaf5c53a
Bikash Das <Bikash.Das1@ibm.com>
2022-08-26 16:52:26 +0530

    CREATCMR-6049

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/SwitzerlandUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/SwitzerlandUtil.java
@@ -60,7 +60,7 @@ public class SwitzerlandUtil extends AutomationUtil {
       CmrConstants.RDC_INSTALL_AT, CmrConstants.RDC_SHIP_TO, CmrConstants.RDC_PAYGO_BILLING);
 
   private static final List<String> NON_RELEVANT_ADDRESS_FIELDS = Arrays.asList("Building", "Floor", "Department", "PostBox", "Attention to/Building/Floor/Office",
-      "Phone #", "FAX", "Customer Name 4");
+      "Att. Person", "Phone #", "FAX", "Customer Name 4");
 
   // private static List<ChMubotyMapping> mubotyMappings = new
   // ArrayList<ChMubotyMapping>();
