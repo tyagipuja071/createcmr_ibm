7a343c68d8f5ad356e14f00079ab24badb11e26b
Jojo DURANTE <durantjp@ph.ibm.com>
2022-09-08 19:02:08 +0800

    Merge pull request #13370 from CI-RDC/CREATCMR-6645_Implemented_warning_msg_popup
    [CREATCMR-6645] - Implemented warning error msg pop-up

--- RequestCMR/WebContent/resources/js/requestentry/requestentry.js
+++ RequestCMR/WebContent/resources/js/requestentry/requestentry.js
@@ -232,7 +232,14 @@ function processRequestAction() {
         // cmr.showModal('addressVerificationModal');
         showAddressVerificationModal();
       }
-    } else {
+    }
+    if((FormManager.getActualValue('vatInd')=='N')&&
+ 		   ((GEOHandler.LA.includes(FormManager.getActualValue('cmrIssuingCntry'))) || 
+ 				   (GEOHandler.EMEA.includes(FormManager.getActualValue('cmrIssuingCntry')))))
+     	{
+     		findVatInd();
+     	}
+    else {
       cmr.showAlert('The request contains errors. Please check the list of errors on the page.');
     }
 
