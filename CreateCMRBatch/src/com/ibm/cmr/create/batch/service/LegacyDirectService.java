18a56a15c20ead9b76e15cdbb7d0ca99f6415c7d
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2022-09-29 03:00:39 -0300

    CREATCMR-5259_Spain - split big records
    Split cmr's with many active records into smaller parts.

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/service/LegacyDirectService.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/service/LegacyDirectService.java
@@ -2555,7 +2555,7 @@ public class LegacyDirectService extends TransConnService {
             response.setMessage("No application ID defined for Country: " + data.getCmrIssuingCntry() + ". Cannot process RDc records.");
           } else {
             try {
-              this.serviceClient.setReadTimeout(60 * 30 * 1000); // 30 mins
+              this.serviceClient.setReadTimeout(60 * 60 * 1000); // 60 mins
               response = this.serviceClient.executeAndWrap(applicationId, request, ProcessResponse.class);
 
               if (response != null && response.getStatus().equals("A")
