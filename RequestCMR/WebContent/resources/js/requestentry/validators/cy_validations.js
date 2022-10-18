b4c6b363ec3b8c489412cc74a55ae34494c0377b
Jian Feng Si <jfengs@cn.ibm.com>
2022-10-18 19:04:14 +0800

    CREATCMR-788 QuotationMarks abbrevNmLoc4
    CREATCMR-788 QuotationMarks abbrevNmLoc4

--- RequestCMR/WebContent/resources/js/requestentry/validators/cy_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/cy_validations.js
@@ -1387,7 +1387,8 @@ function abbrvLocMandatory() {
   interval = setInterval(function() {
     var role = FormManager.getActualValue('userRole').toUpperCase();
     var viewOnlyPage = FormManager.getActualValue('viewOnlyPage');
-
+    FormManager.addValidator('abbrevNm', Validators.NO_QUOTATION, [ 'Abbreviated Name (TELX1)' ]);
+    FormManager.addValidator('abbrevLocn', Validators.NO_QUOTATION, [ 'Abbreviated Location' ]);
     if (viewOnlyPage != 'true') {
       if (role != 'REQUESTER') {
         FormManager.addValidator('abbrevLocn', Validators.REQUIRED, [ 'AbbrevLocation' ], 'MAIN_CUST_TAB');
@@ -1401,6 +1402,8 @@ function abbrvLocMandatory() {
 
 function abbrvLocMandatoryOnChange() {
   var role = FormManager.getActualValue('userRole').toUpperCase();
+  FormManager.addValidator('abbrevNm', Validators.NO_QUOTATION, [ 'Abbreviated Name (TELX1)' ]);
+  FormManager.addValidator('abbrevLocn', Validators.NO_QUOTATION, [ 'Abbreviated Location' ]);
   dojo.connect(FormManager.getField('abbrevLocn'), 'onChange', function(value) {
     if (role != 'REQUESTER') {
       FormManager.addValidator('abbrevLocn', Validators.REQUIRED, [ 'AbbrevLocation' ], 'MAIN_CUST_TAB');
