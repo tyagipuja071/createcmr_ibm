0f09dec867b19608675fa168b8d0e5b79ac033ea
Bikash Das <Bikash.Das1@ibm.com>
2022-10-30 09:46:04 +0530

    CREATCMR-5984 - Changes for Spain Enterprise calculation.

---
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/SpainFieldsContainer.java
@@ -0,0 +1,33 @@
+package com.ibm.cio.cmr.request.automation.util;
+
+public class SpainFieldsContainer {
+
+  String enterprise;
+  String isuCd;
+  String clientTier;
+
+  public String getEnterprise() {
+    return enterprise;
+  }
+
+  public void setEnterprise(String enterprise) {
+    this.enterprise = enterprise;
+  }
+
+  public String getIsuCd() {
+    return isuCd;
+  }
+
+  public void setIsuCd(String isuCd) {
+    this.isuCd = isuCd;
+  }
+
+  public String getClientTier() {
+    return clientTier;
+  }
+
+  public void setClientTier(String clientTier) {
+    this.clientTier = clientTier;
+  }
+
+}
