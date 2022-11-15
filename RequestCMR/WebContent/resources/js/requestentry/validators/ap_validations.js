9ce5d561385acdd857561e7a618355509c4c225a
Joyce Ann Barcelona <barcelj@ph.ibm.com>
2022-11-15 22:37:24 +0800

    CREATCMR-7473: Stop submit on Update if no changes

--- RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
@@ -4228,10 +4228,7 @@ function executeBeforeSubmit() {
     if (reqType == 'U') {
       var errMsg = checkAnyChangesOnCustNameAddrGST();
       if (errMsg != '' && action == 'SFP') {
-        cmr.showConfirm('showVerificationModal()', errMsg, 'Warning', null, {
-          OK : 'Yes',
-          CANCEL : 'No'
-        });
+        cmr.showAlert(errMsg);
       } else {
         showVerificationModal();
       }
@@ -4280,7 +4277,7 @@ function checkAnyChangesOnCustNameAddrGST() {
     }
   }
   if (!isUpdated) {
-    errorMsg = 'You haven\'t updated anything on customer name/address or GST#, please check and take relevant edit operation before submit this Update request. Proceed?';
+    errorMsg = 'You haven\'t updated anything on customer name/address or GST#, please check and take relevant edit operation before submit this Update request.';
   }
   return errorMsg;
 }
