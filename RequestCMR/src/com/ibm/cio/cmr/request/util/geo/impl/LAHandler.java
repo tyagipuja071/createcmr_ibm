afd835f960c61da2479e9d702a85df938a957be8
Rochelle Salazar <salazarm@ph.ibm.com>
2022-11-15 20:41:00 +0800

    CREATCMR-7337 la mvp3 upload validate

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/LAHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/LAHandler.java
@@ -4215,13 +4215,13 @@ public class LAHandler extends GEOHandler {
     String strRequesterId = admin.getRequesterId().toLowerCase();
     String groups = SystemParameters.getString("LA_CME_GSI_LIST");
     LOG.debug("strRequesterId : " + strRequesterId);
-    LOG.debug("groups cmde/gsi : " + strRequesterId);
+    LOG.debug("groups cmde/gsi : " + groups);
 
     requesterFromCmdeGsi = BluePagesHelper.isUserInLaBlueGroup(strRequesterId, groups);
     LOG.debug("requester from cmde/gsi : " + requesterFromCmdeGsi);
 
     groups = SystemParameters.getString("LA_AR_LIST");
-    LOG.debug("groups ar : " + strRequesterId);
+    LOG.debug("groups ar : " + groups);
     requesterFromAcctReciv = BluePagesHelper.isUserInLaBlueGroup(strRequesterId, groups);
     LOG.debug("requester from acct receivable : " + requesterFromAcctReciv);
 
