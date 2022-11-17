0d893242f6bbaf95914f8674a0600eb0e5d68158
Jojo DURANTE <durantjp@ph.ibm.com>
2022-11-17 17:38:18 +0800

    Merge pull request #14117 from CI-RDC/CREATCMR-7667_WARNINGMSG
    CREATCMR-7667_WARNINGMSG

--- RequestCMR/WebContent/resources/js/requestentry/validators/ww_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ww_validations.js
@@ -1026,9 +1026,9 @@ function findVatInd() {
 	    if (vatInd == 'N') {
 	      console.log("Test");
 	     cmr.showConfirm('markSendForProcessing()',
-	          '<div align="center"><strong><i><u><b><p style="font-size:25px"> Warning Message</p></u><br><br><p style="font-size:15px">Please note, if you choose not to provide the company VAT ID, IBM will not be able to include VAT ID in the customer address section. As a consequence the IBM invoice may not be eligible to recover the VAT charged to the client which can cause a delay on payment in countries that is required. However, at any moment business can submit VAT ID update whenever VAT ID is collected needed.</p><br><br> <p style="font-size:17px">Would you like  to proceed?</p></i></strong></div>', 'Warning', null, {
-	    		OK : 'Ok',
-	    CANCEL : 'Cancel'
+	          '<div align="center"><strong><i><u><b><p style="font-size:25px"> Warning Message</p></u><br><br><p style="font-size:15px">Please note, if you choose not to provide the company’s VAT ID, IBM will not be able to include VAT ID in the customer address section. As a consequence the IBM invoice may not be eligible to recover the VAT charged to the client which can cause a delay on payment in countries that is required. However, at any moment business can submit VAT ID update whenever VAT ID is collected/needed.</p><br><br> <p style="font-size:17px">Would you like  to proceed?</p></i></strong></div>', 'Warning', null, {
+	    		OK : 'YES',
+	    CANCEL : 'NO'
 	    	  });
 	    }
 }
