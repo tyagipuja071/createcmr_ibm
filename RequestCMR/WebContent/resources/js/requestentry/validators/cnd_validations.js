ab693690d49a10b58f00d68100cced30b977399b
Tarishi Geetey <Tarishi.Geetey@ibm.com>
2022-12-02 14:51:34 +0530

    CREATCMR-5871 req summary update cnd and default for creates PM

--- RequestCMR/WebContent/resources/js/requestentry/validators/cnd_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/cnd_validations.js
@@ -48,6 +48,7 @@ function modeOfPaymentAndOrderBlockCdHandling() {
     // Code' ], 'MAIN_CUST_TAB');
   } else {
     FormManager.resetValidations('modeOfPayment');
+    FormManager.setValue('modeOfPayment', 'A001');
     FormManager.resetValidations('ordBlk');
     FormManager.readOnly('modeOfPayment');
     FormManager.readOnly('ordBlk');
