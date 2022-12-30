d0fdf77a2501b063a79fbb22d260329f91e38917
Vagbhi Sharma <Vagbhi.Sharma@ibm.com>
2022-12-30 11:33:33 +0530

    CREATCMR-7987

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/FranceUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/FranceUtil.java
@@ -1336,7 +1336,7 @@ public class FranceUtil extends AutomationUtil {
       engineData.addNegativeCheckStatus("DUPLICATE_CHECK_ERROR", "Duplicate CMR check using customer name match failed to execute.");
       break;
     case NoIBMRecord:
-      if (scenario.equalsIgnoreCase(SCENARIO_IBM_EMPLOYEE)) {
+      if (SCENARIO_IBM_EMPLOYEE.equalsIgnoreCase(scenario)) {
         engineData.addRejectionComment("OTH", "Employee details not found in IBM BluePages.", "", "");
         details.append("Employee details not found in IBM BluePages.").append("\n");
         return false;
