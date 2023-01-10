4f7a716f4d22b2c0ab68e0d7d056921e0c889a46
Fernando Aires <Fernando.Machado.Aires.De.Oliveira@ibm.com>
2023-01-10 19:22:40 -0300

    CREATCMR-8052_VatUpdIssues_main

--- RequestCMR/WebContent/resources/js/requestentry/requestentry.js
+++ RequestCMR/WebContent/resources/js/requestentry/requestentry.js
@@ -142,7 +142,7 @@ function processRequestAction() {
     } else if (FormManager.validate('frmCMR') && checkIfDataOrAddressFieldsUpdated(frmCMR)) {
       cmr.showAlert('Request cannot be submitted for update because No data/address changes made on request. ');
     } else if (FormManager.validate('frmCMR') && !comp_proof_INAUSG) {
-      if ((GEOHandler.GROUP1.includes(FormManager.getActualValue('cmrIssuingCntry')) || NORDX.includes(FormManager.getActualValue('cmrIssuingCntry'))) && (vatInd == 'N') && (custGrp != 'CROSS')) {
+      if ((GEOHandler.GROUP1.includes(FormManager.getActualValue('cmrIssuingCntry')) || NORDX.includes(FormManager.getActualValue('cmrIssuingCntry'))) && (vatInd == 'N') && (custGrp != 'CROSS') && (reqType != 'U')) {
         findVatInd();
       } else if (checkForConfirmationAttachments()) { 
         showDocTypeConfirmDialog();
