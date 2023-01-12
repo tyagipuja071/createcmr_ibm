b0ef4ae7ae59b505e15744c6e2b91bb393d4a621
Puja Bhatt <puja.bhatt@ibm.com>
2023-01-12 19:48:43 +0530

    [CREATCMR-7991]-Fix for warning msg for create req

--- RequestCMR/WebContent/resources/js/requestentry/requestentry.js
+++ RequestCMR/WebContent/resources/js/requestentry/requestentry.js
@@ -150,7 +150,7 @@ function processRequestAction() {
       cmr.showAlert('Request cannot be submitted for update because No data/address changes made on request. ');
     } else if (FormManager.validate('frmCMR') && !comp_proof_INAUSG) {
       if ((GEOHandler.GROUP1.includes(FormManager.getActualValue('cmrIssuingCntry')) || NORDX.includes(FormManager.getActualValue('cmrIssuingCntry'))) && (vatInd == 'N') && (custGrp != 'CROSS')
-          && ((oldVatValue=='' && reqType=='U') || (oldVatValue=='' && reqType=='C'))) {
+          && ((oldVatValue=='' && reqType=='U') || (reqType=='C'))) {
         findVatInd();
       } else if (checkForConfirmationAttachments()) {
         showDocTypeConfirmDialog();
