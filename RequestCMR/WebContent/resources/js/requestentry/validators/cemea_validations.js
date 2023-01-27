19844ecd31720a3c1a03a44b5acf9c20412becaf
Ayushi . <AYUSHI.Ayushi@ibm.com>
2023-01-27 15:04:41 +0530

    CREATCMR 8226

--- RequestCMR/WebContent/resources/js/requestentry/validators/cemea_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/cemea_validations.js
@@ -2358,7 +2358,7 @@ function setSBOValues() {
     if (dijit.byId('cisServiceCustIndc').get('checked')) {
       FormManager.setValue('salesBusOffCd', 'R04');
     } else if (custSubType == 'XCOM' || custSubType == 'XTP') {
-      FormManager.setValue('salesBusOffCd', 'RO4');
+      FormManager.setValue('salesBusOffCd', 'R04');
     } else if (custSubType == 'XBP') {
       FormManager.setValue('salesBusOffCd', '000');
     } else if (custSubType == 'XINT') {
