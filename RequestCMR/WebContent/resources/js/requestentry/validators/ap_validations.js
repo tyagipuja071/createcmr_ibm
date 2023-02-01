d1cd074321697848534a5f05ffb4d6528c08fb8a
Jian Feng Si <jfengs@cn.ibm.com>
2023-02-01 16:03:45 +0800

    CREATCMR-7886 Coverage2023 lock id my sg
    CREATCMR-7886 Coverage2023 lock id my sg

--- RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
@@ -5877,7 +5877,7 @@ function setCTCIsuByClusterIndonesia() {
 function lockInacForIDMYSG() {
 
   var custSubGrp = FormManager.getActualValue('custSubGrp');
-  if (custSubGrp == 'DUMMY' || custSubGrp == 'PRIV' || custSubGrp == 'INTER' ) {
+  if (custSubGrp == 'DUMMY' || custSubGrp == 'PRIV' || custSubGrp == 'PRICU' || custSubGrp == 'INTER' ) {
     FormManager.setValue('inacCd','');
     FormManager.setValue('inacType', '');
     FormManager.readOnly('inacCd');
