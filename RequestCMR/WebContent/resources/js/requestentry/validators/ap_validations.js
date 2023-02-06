bda3fae7b9864bd8de8791a913ebc9e738ec2b71
Xiang Bin Liu <dlliuxb@cn.ibm.com>
2023-02-06 15:29:51 +0800

    CREATCMR-7880_HK_clientTier_obsoletedValidation_fix_main

--- RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
@@ -4727,6 +4727,9 @@ function addCtcObsoleteValidator() {
               return new ValidationResult(null, true);
             }
           }
+          if(clientTier == "T" && (cntry == '736' || cntry == '738')){
+            return new ValidationResult(null, true);
+          }
           if(clientTier == "T" && cntry == '616'){
           	console.log('>>> Skip CTC Obsolete Validator clientTier = T for AU');
             return new ValidationResult(null, true);
@@ -4736,13 +4739,16 @@ function addCtcObsoleteValidator() {
             return new ValidationResult(null, true);
           }
           return new ValidationResult(null, false, 'Client tier is obsoleted. Please select valid value from list.');
-          } else if (reqType == 'U' && oldCtc != null && oldCtc != clientTier && (clientTier == "4" ||clientTier == "6"|| clientTier == "A" || clientTier == "B" ||clientTier == "M"|| clientTier == "V" || clientTier == "T" || clientTier == "S" || clientTier == "N" || clientTier == "C")) {
-           return new ValidationResult(null, false, 'Client tier is obsoleted. Please select valid Client tier value from list.');
-           } else {
-             return new ValidationResult(null, true);
-            }
+        } else if (reqType == 'U' && oldCtc != null && oldCtc != clientTier && (clientTier == "4" ||clientTier == "6"|| clientTier == "A" || clientTier == "B" ||clientTier == "M"|| clientTier == "V" || clientTier == "T" || clientTier == "S" || clientTier == "N" || clientTier == "C")) {
+          if(clientTier == "T" && (cntry == '736' || cntry == '738')){
+            return new ValidationResult(null, true);
           }
+          return new ValidationResult(null, false, 'Client tier is obsoleted. Please select valid Client tier value from list.');
+        } else {
+          return new ValidationResult(null, true);
         }
+      }
+     }
     })(), 'MAIN_IBM_TAB', 'frmCMR');
  }
 
