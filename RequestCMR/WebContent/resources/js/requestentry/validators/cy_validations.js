1ace66f80472a333fd0ce049263d9778856e423b
Aarthi <Aarthi.V@ibm.com>
2023-02-09 13:25:09 +0530

    CREATCMR-7856_CY_defect_fix

--- RequestCMR/WebContent/resources/js/requestentry/validators/cy_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/cy_validations.js
@@ -3324,5 +3324,6 @@ dojo.addOnLoad(function() {
   GEOHandler.addAfterTemplateLoad(addISUHandler, [ SysLoc.CYPRUS ]);
   GEOHandler.addAfterConfig(addISUHandler, [ SysLoc.CYPRUS ]);
   GEOHandler.registerValidator(checkCmrUpdateBeforeImport, [ SysLoc.CYPRUS ], null, true);
-
+  GEOHandler.addAfterTemplateLoad(addVATDisabler, [ SysLoc.CYPRUS ]);
+  GEOHandler.addAfterConfig(addVATDisabler, [ SysLoc.CYPRUS ]);
 });
