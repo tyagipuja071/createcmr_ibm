e86e4a3b04cb0a7a5abe9043f76b2da85a1bef10
Seff Malonda <joseph.malonda@ibm.com>
2023-02-10 15:02:07 +0800

    CREATCMR-8126_yellow_la Update State/Prov handling for BR

--- RequestCMR/src/com/ibm/cio/cmr/request/service/DropDownService.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/service/DropDownService.java
@@ -284,19 +284,6 @@ public class DropDownService extends BaseSimpleService<DropdownModel> {
         query.append("  and ((REFT_COUNTRY_KEY = (select REFT_COUNTRY_KEY from CMMA.REFT_COUNTRY_W where COUNTRY_CD = :LAND1) and 'US' = :LAND1)");
         query.append("  or (REFT_COUNTRY_KEY = 0 and 'US' <> :LAND1))");
         query.setParameter("LAND1", params.getParam("landCntry"));
-      } else if (LAHandler.isBRIssuingCountry(cntry)) {
-        String landCntryLA = PageManager.getDefaultLandedCountry(cntry);
-
-        if (!"BR".equals(params.getParam("landCntry"))) {
-          query
-              .append(" and STATE_PROV_CD = 'EX' and REFT_COUNTRY_KEY  = (select REFT_COUNTRY_KEY from CMMA.REFT_COUNTRY_W where COUNTRY_CD = 'BR')");
-        } else {
-          query.append("  and ((REFT_COUNTRY_KEY = (select REFT_COUNTRY_KEY from CMMA.REFT_COUNTRY_W where COUNTRY_CD = :LAND1) and '" + landCntryLA
-              + "' = :LAND1)");
-          query.append("  or (REFT_COUNTRY_KEY = 0 and '" + landCntryLA + "' <> :LAND1))");
-          query.setParameter("LAND1", params.getParam("landCntry"));
-        }
-
       } else if ("758".equalsIgnoreCase(cntry)) {
         // String dropDownCity = params.getParam("dropDownCity") != null ?
         // params.getParam("dropDownCity").toString() : "";
@@ -570,7 +557,7 @@ public class DropDownService extends BaseSimpleService<DropdownModel> {
     }
 
     if ("DropDownCity".equalsIgnoreCase(fieldId)) {
-      if (LAHandler.isBRIssuingCountry(cntry) || CNHandler.isCNIssuingCountry(cntry)) {
+      if (CNHandler.isCNIssuingCountry(cntry)) {
         String param = params.getParam("stateProv") != null ? params.getParam("stateProv").toString() : "";
         query.append(" and CITY_ID like('" + param + "%')");
         query.append(" and (ISSUING_CNTRY = '" + cntry + "')");
