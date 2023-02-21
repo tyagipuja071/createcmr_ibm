25e4ecf56f1c250c7a492457d06d9947120c666c
Vagbhi Sharma <Vagbhi.Sharma@ibm.com>
2023-02-21 14:54:19 +0530

    CREATCMR-7892

--- RequestCMR/WebContent/resources/js/requestentry/validators/kr_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/kr_validations.js
@@ -24,6 +24,7 @@ function afterConfigKR() {
     FormManager.setValue('inacCd','');
     FormManager.enable('searchTerm');
     setSearchTermDropdownValues();
+    LockDefaultISUClientTierMrcValues();
     FormManager.readOnly('clientTier');
     FormManager.readOnly('isuCd');
     FormManager.readOnly('mrcCd');
@@ -620,6 +621,10 @@ function LockDefaultISUClientTierMrcValues() {
     FormManager.setValue('isuCd', '5K');
     FormManager.readOnly('isuCd');
   }
+  
+  if (searchTerm == '04461' || searchTerm == '04466' || searchTerm == '05223' ) {
+    getIsuFromIsic();
+  }
   FormManager.readOnly('clientTier');
   FormManager.readOnly('isuCd');
   FormManager.readOnly('mrcCd');
@@ -710,12 +715,14 @@ function validateCustnameForKynd() {
 
 function getIsuFromIsic(){
 	  var isicCd = FormManager.getActualValue('isicCd');
-	  
+	  if (!(searchTerm == '04461' || searchTerm == '04466' || searchTerm == '05223')) {
+		    return;
+      }
 	  var ISU = [];
 	  if (isicCd != '') {
 	    var qParams = {
 	      _qall : 'Y',
-	      ISSUING_CNTRY : cmrIssuingCntry,
+	      ISSUING_CNTRY : '856',
 	      REP_TEAM_CD : '%' + isicCd + '%'
 	    };
 	    var results = cmr.query('GET.ISULIST.BYISIC', qParams);
