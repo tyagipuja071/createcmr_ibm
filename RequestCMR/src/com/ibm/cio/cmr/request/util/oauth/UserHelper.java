aa937e99516b0d41c635ef29661d33fceb3a8ecd
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2023-03-13 08:47:58 -0300

    CREATCMR-8616_SSO_CreateCMR
    reformating the UserHelper

--- RequestCMR/src/com/ibm/cio/cmr/request/util/oauth/UserHelper.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/oauth/UserHelper.java
@@ -13,174 +13,170 @@ import com.ibm.websphere.security.auth.WSSubject;
 import com.ibm.websphere.security.cred.WSCredential;
 
 public class UserHelper {
-  private static final String BLUEID_ATTR_UNID = "uniqueSecurityName";
-  private static final String BLUEID_ATTR_EMAIL = "email";
-  Subject _s;
+	private static final String BLUEID_ATTR_UNID = "uniqueSecurityName";
+	private static final String BLUEID_ATTR_EMAIL = "email";
+	Subject _s;
 
-  public UserHelper() {
-    try {
-      _s = WSSubject.getCallerSubject();
-    } catch (WSSecurityException e) {
-      e.printStackTrace();
-      _s = null;
-    }
-  }
+	public UserHelper() {
+		try {
+			_s = WSSubject.getCallerSubject();
+		} catch (WSSecurityException e) {
+			e.printStackTrace();
+			_s = null;
+		}
+	}
 
-  boolean isAuthenticated() {
-    boolean result = false;
+	boolean isAuthenticated() {
+		boolean result = false;
+		/*
+		 * if there is a subject and the public credentials of that subject
+		 * contains at least one WSCredential that is not unauthenticated, then
+		 * return true
+		 */
+		if (_s != null) {
+			Set<WSCredential> publicWSCreds = _s.getPublicCredentials(WSCredential.class);
+			if (publicWSCreds != null) {
+				for (Iterator<WSCredential> i = publicWSCreds.iterator(); !result && i.hasNext();) {
+					WSCredential cred = i.next();
+					result = !cred.isUnauthenticated();
+				}
+			}
+		}
+		return result;
+	}
 
-    /*
-     * if there is a subject and the public credentials of that subject contains
-     * at least one WSCredential that is not unauthenticated, then return true
-     */
-    if (_s != null) {
-      Set<WSCredential> publicWSCreds = _s.getPublicCredentials(WSCredential.class);
-      if (publicWSCreds != null) {
-        for (Iterator<WSCredential> i = publicWSCreds.iterator(); !result && i.hasNext();) {
-          WSCredential cred = i.next();
-          result = !cred.isUnauthenticated();
-        }
-      }
-    }
-    return result;
-  }
+	public String getPrincipalName() {
+		String result = null;
+		if (_s != null) {
+			Set<Principal> principals = _s.getPrincipals();
+			if (principals != null && principals.size() > 0) {
+				result = principals.iterator().next().getName();
+			}
+		}
+		return result;
+	}
 
-  public String getPrincipalName() {
-    String result = null;
-    if (_s != null) {
-      Set<Principal> principals = _s.getPrincipals();
-      if (principals != null && principals.size() > 0) {
-        result = principals.iterator().next().getName();
-      }
-    }
-    return result;
-  }
+	public SimpleJWT getIDToken() {
+		SimpleJWT result = null;
+		/*
+		 * Look for a private credential of type java.util.Hashtable, and then
+		 * within there for an entry with name "id_token". This was
+		 * reverse-engineered from looking at dump.jsp
+		 */
+		String idtokenStr = getPrivateHashtableAttr("id_token");
+		if (idtokenStr != null) {
+			try {
+				result = new SimpleJWT(idtokenStr);
+			} catch (Exception e) {
+				e.printStackTrace();
+				result = null;
+			}
+		}
 
-  public SimpleJWT getIDToken() {
-    SimpleJWT result = null;
+		return result;
+	}
 
-    /*
-     * Look for a private credential of type java.util.Hashtable, and then
-     * within there for an entry with name "id_token". This was
-     * reverse-engineered from looking at dump.jsp
-     */
-    String idtokenStr = getPrivateHashtableAttr("id_token");
-    if (idtokenStr != null) {
-      try {
-        result = new SimpleJWT(idtokenStr);
-      } catch (Exception e) {
-        e.printStackTrace();
-        result = null;
-      }
-    }
+	public String getAccessToken() {
+		/*
+		 * Look for a private credential of type java.util.Hashtable, and then
+		 * within there for an entry with name "access_token". This was
+		 * reverse-engineered from looking at dump.jsp
+		 */
+		String result = getPrivateHashtableAttr("access_token");
 
-    return result;
-  }
+		return result;
+	}
 
-  public String getAccessToken() {
-    /*
-     * Look for a private credential of type java.util.Hashtable, and then
-     * within there for an entry with name "access_token". This was
-     * reverse-engineered from looking at dump.jsp
-     */
-    String result = getPrivateHashtableAttr("access_token");
+	protected String getPrivateHashtableAttr(String attrName) {
+		String result = null;
+		if (_s != null) {
+			@SuppressWarnings("rawtypes")
+			Set<Hashtable> privateHTs = _s.getPrivateCredentials(Hashtable.class);
+			if (privateHTs != null && privateHTs.size() > 0) {
+				result = (String) privateHTs.iterator().next().get(attrName);
+			}
+		}
+		return result;
+	}
 
-    return result;
-  }
+	protected String getIDTokenClaimsValue(String idTokenClaimsAttrName) {
+		String result = null;
 
-  protected String getPrivateHashtableAttr(String attrName) {
-    String result = null;
-    if (_s != null) {
-      @SuppressWarnings("rawtypes")
-      Set<Hashtable> privateHTs = _s.getPrivateCredentials(Hashtable.class);
-      if (privateHTs != null && privateHTs.size() > 0) {
-        result = (String) privateHTs.iterator().next().get(attrName);
-      }
-    }
-    return result;
-  }
+		SimpleJWT idtoken = getIDToken();
+		if (idtoken != null) {
+			JSONObject claims = idtoken.getClaims();
+			if (claims != null) {
+				result = (String) claims.get(idTokenClaimsAttrName);
+			}
+		}
 
-  protected String getIDTokenClaimsValue(String idTokenClaimsAttrName) {
-    String result = null;
+		return result;
+	}
 
-    SimpleJWT idtoken = getIDToken();
-    if (idtoken != null) {
-      JSONObject claims = idtoken.getClaims();
-      if (claims != null) {
-        result = (String) claims.get(idTokenClaimsAttrName);
-      }
-    }
+	public String getEmailAddress() {
+		String result = null;
+		/*
+		 * Where the email address is located is going to depend somehwat on how
+		 * the user authenticated. This method tries to be a little clever about
+		 * figuring that out.
+		 */
+		// first try email claim
+		result = getIDTokenClaimsValue(BLUEID_ATTR_EMAIL);
 
-    return result;
-  }
+		if (result == null) {
+			// others to follow?
+		}
 
-  public String getEmailAddress() {
-    String result = null;
+		return result;
+	}
 
-    /*
-     * Where the email address is located is going to depend somehwat on how the
-     * user authenticated. This method tries to be a little clever about
-     * figuring that out.
-     */
+	public String getUNID() {
+		String result = null;
 
-    // first try email claim
-    result = getIDTokenClaimsValue(BLUEID_ATTR_EMAIL);
+		/*
+		 * Where the email address is located is going to depend somehwat on how
+		 * the user authenticated. This method tries to be a little clever about
+		 * figuring that out.
+		 */
+		// first try email claim
+		result = getIDTokenClaimsValue(BLUEID_ATTR_UNID);
 
-    if (result == null) {
-      // others to follow?
-    }
+		if (result == null) {
+			// others to follow?
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  public String getUNID() {
-    String result = null;
+	public String getDisplayName() {
+		/*
+		 * Look for an idtoken claim called displayName. If not found, use
+		 * principal name
+		 */
+		String result = null;
 
-    /*
-     * Where the email address is located is going to depend somehwat on how the
-     * user authenticated. This method tries to be a little clever about
-     * figuring that out.
-     */
+		result = getIDTokenClaimsValue("displayName");
 
-    // first try email claim
-    result = getIDTokenClaimsValue(BLUEID_ATTR_UNID);
+		if (result == null) {
+			result = getPrincipalName();
+		}
 
-    if (result == null) {
-      // others to follow?
-    }
+		return result;
+	}
 
-    return result;
-  }
+	public String getRegistrationId() {
+		/*
+		 * Look for an idtoken claim called sub. If not found, use preferred
+		 * username
+		 */
+		String result = null;
 
-  public String getDisplayName() {
-    /*
-     * Look for an idtoken claim called displayName. If not found, use principal
-     * name
-     */
-    String result = null;
+		result = getIDTokenClaimsValue("sub");
 
-    result = getIDTokenClaimsValue("displayName");
+		if (result == null) {
+			result = getIDTokenClaimsValue("preferred_username");
+		}
 
-    if (result == null) {
-      result = getPrincipalName();
-    }
-
-    return result;
-  }
-
-  public String getRegistrationId() {
-    /*
-     * Look for an idtoken claim called sub. If not found, use preferred
-     * username
-     */
-    String result = null;
-
-    result = getIDTokenClaimsValue("sub");
-
-    if (result == null) {
-      result = getIDTokenClaimsValue("preferred_username");
-    }
-
-    return result;
-  }
+		return result;
+	}
 }
