32ebae948773bf0a270ea114dbd3773bed6daca0
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2023-03-16 08:16:59 -0300

    CREATCMR-8616_SSO_CreateCMR

--- RequestCMR/src/com/ibm/cio/cmr/request/controller/login/LoginController.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/controller/login/LoginController.java
@@ -624,9 +624,10 @@ public class LoginController extends BaseController {
 
 				session.setAttribute("loginUser", loginUser);
 				HttpServletResponse resp = response;
-				resp.sendRedirect("/CreateCMR/setUserRolesAndPermissions");
+				resp.sendRedirect(resp.encodeRedirectURL(
+						SystemConfiguration.getValue("APPLICATION_URL") + "/setUserRolesAndPermissions"));
 
-				// req.getRequestDispatcher("/setUserRolesAndPermissions").forward(req,
+				// request.getRequestDispatcher("/setUserRolesAndPermissions").forward(request,
 				// response);
 				// filterChain.doFilter(req, resp);
 				return;
@@ -643,6 +644,10 @@ public class LoginController extends BaseController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		// catch (ServletException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 		return;
 	}
 
@@ -835,12 +840,12 @@ public class LoginController extends BaseController {
 						} else if (decoded.startsWith("r")) {
 							mv = new ModelAndView("redirect:/request?" + params, "appUser", appUser);
 						} else {
-							mv = new ModelAndView("redirect:/home", "appUser", appUser);
+							mv = new ModelAndView("forward:/home", "appUser", appUser);
 						}
 					} else if (appUser.isApprover()) {
 						mv = new ModelAndView("redirect:/myappr", "approval", new MyApprovalsModel());
 					} else {
-						mv = new ModelAndView("redirect:/home", "appUser", appUser);
+						mv = new ModelAndView("forward:/home", "appUser", appUser);
 					}
 					// setPageKeys("HOME", "OVERVIEW", mv);
 				} else {
