c52728f453d83e3b88cece53a707c8f8d80022ea
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2023-03-20 11:45:05 -0300

    CREATCMR-8616_CreateCMR_SSO

--- RequestCMR/src/com/ibm/cio/cmr/request/filter/AppUserInjectFilter.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/filter/AppUserInjectFilter.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
 import java.util.Set;
 
 import javax.servlet.Filter;
@@ -24,10 +21,8 @@ import org.apache.log4j.Logger;
 
 import com.ibm.cio.cmr.request.CmrConstants;
 import com.ibm.cio.cmr.request.config.SystemConfiguration;
-import com.ibm.cio.cmr.request.model.login.LogInUserModel;
 import com.ibm.cio.cmr.request.user.AppUser;
 import com.ibm.cio.cmr.request.util.oauth.OAuthUtils;
-import com.ibm.cio.cmr.request.util.oauth.Tokens;
 
 /**
  * This filter ensures valid user is present at the request. If not, the request
@@ -82,64 +77,6 @@ public class AppUserInjectFilter implements Filter {
 						return;
 					}
 
-					// when request contains code and grant_id, means we are at
-					// second step of SSO and need to get access token
-					if (paramNames.contains("code") && paramNames.contains("grant_id")) {
-						LOG.trace("Requesting access token for grant_id: " + request.getParameter("grant_id"));
-
-						// get access_token and id_token
-						String code = request.getParameter("code");
-						String rawToken = OAuthUtils.getAccessToken(code);
-
-						// parse raw token
-						Tokens tokens = OAuthUtils.parseToken(rawToken);
-
-						// validate JWT signature
-						boolean isJWTValid = false;
-						try {
-							LOG.trace("Validating signature for grant_id: " + request.getParameter("grant_id"));
-							isJWTValid = OAuthUtils.validateSignature();
-							LOG.trace("JWT Signature validated successfully for grant_id: "
-									+ request.getParameter("grant_id"));
-						} catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
-							// TODO interrupt the thread here
-							LOG.error("An error occured when validating the signature: ", e);
-						}
-
-						// assign AppUser
-						if (isJWTValid) {
-							userIntranetEmail = (String) tokens.getId_token().getClaims().get("emailAddress");
-							session.setAttribute("userIntranetEmail", userIntranetEmail);
-
-							LogInUserModel loginUser = new LogInUserModel();
-							loginUser.setUsername(userIntranetEmail);
-
-							// new OAuthUtils().authorizeAndSetRoles(loginUser,
-							// userService, req, resp);
-
-							session.setAttribute("loggedInUserModel", loginUser);
-							session.setAttribute("accessToken", tokens.getAccess_token());
-							session.setAttribute("tokenExpiringTime", tokens.getExpires_in());
-
-							session.setAttribute("loginUser", loginUser);
-							HttpServletResponse resp = (HttpServletResponse) response;
-							resp.sendRedirect("/CreateCMR/setUserRolesAndPermissions");
-
-							// req.getRequestDispatcher("/setUserRolesAndPermissions").forward(req,
-							// response);
-							// filterChain.doFilter(req, resp);
-							return;
-						} else {
-							LOG.trace("Invalid Token! Unable to proceed with the request.");
-							// TODO what to do if token is invalid or expired?
-							OAuthUtils.revokeToken(tokens.getAccess_token());
-							AppUser.remove(req);
-							session.invalidate();
-							filterChain.doFilter(req, response);
-							return;
-						}
-					}
-
 					LOG.debug("Redirecting to W3 ID Provisioner...");
 					HttpServletResponse httpResp = (HttpServletResponse) response;
 					session.invalidate();
@@ -175,6 +112,11 @@ public class AppUserInjectFilter implements Filter {
 			return false;
 		}
 		if (url.endsWith("/external.json")) {
+			return false;
+		}
+
+		if (request.getParameterMap().keySet().contains("errorMessage") && url.endsWith("/login")) {
+
 			return false;
 		}
 
