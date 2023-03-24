afa72e2e75d33719cb9c0988e55662031edb14fc
Jian Feng Si <jfengs@cn.ibm.com>
2023-03-24 13:31:16 +0800

    CREATCMR-8799 bss zp01 seqNo for upd 20x
    CREATCMR-8799 bss zp01 seqNo for upd 20x

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/AustraliaUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/AustraliaUtil.java
@@ -410,7 +410,7 @@ public class AustraliaUtil extends AutomationUtil {
     if ("U".equals(admin.getReqType()) && ("PayGo-Test".equals(admin.getSourceSystId()) || "BSS".equals(admin.getSourceSystId()))) {
         Addr pg01 = requestData.getAddress("PG01");
         if(pg01 != null){
-        	checkANZPaygoAddr(entityManager, data.getId().getReqId());
+        	// checkANZPaygoAddr(entityManager, data.getId().getReqId());
         }
       }
     
