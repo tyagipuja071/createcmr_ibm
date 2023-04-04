63fddc66d6fe58ab7289009b568c620c8ff6e157
vinod-kumar-saharma <117459460+vinod-kumar-saharma@users.noreply.github.com>
2023-04-04 09:13:12 +0530

    CREATCMR-8755_IN_ABB name issue of Acquisition and ESA

--- RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ap_validations.js
@@ -1355,7 +1355,8 @@ function autoSetAbbrevNmLocnLogic() {
       } else if (custSubGrp == "SOFT" || custSubGrp == "XSOFT") {
         _abbrevNm = "SOFTLAYER USE ONLY";
       } else if (custSubGrp == "ESOSW" || custSubGrp == "XESO") {
-        _abbrevNm = "ESA/OEM/SWG_" + custNm1;
+        // _abbrevNm = "ESA/OEM/SWG_" + custNm1;
+        _abbrevNm = "ESA Use Only";
       } else if (custSubGrp == "AQSTN") {
         _abbrevNm ="Acquisition Use Only";
       } else {
