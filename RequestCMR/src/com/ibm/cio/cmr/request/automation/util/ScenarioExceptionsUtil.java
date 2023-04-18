d01d7fefcf086542b42bdb5344e1c97f6d2b35a7
JOYCE ANN BARCELONA <barcelj@ph.ibm.com>
2023-04-18 10:06:52 +0800

    Update ScenarioExceptionsUtil.java

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/ScenarioExceptionsUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/ScenarioExceptionsUtil.java
@@ -40,7 +40,6 @@ public class ScenarioExceptionsUtil {
   boolean manualReviewIndc;
   boolean reviewExtReqIndc;
   boolean skipFindGbgForPrivates;
- 
   public ScenarioExceptionsUtil(EntityManager entityManager, String cmrIssuingCntry, String subRegion, String scenario, String subScenario) {
 
     String custSubGroup = subScenario;
