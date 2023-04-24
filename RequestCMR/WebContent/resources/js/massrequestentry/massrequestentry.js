6466007f726f198313f22a134cb28e3585df2a71
Seff Malonda <joseph.malonda@ibm.com>
2023-04-24 17:36:33 +0800

    CREATCMR-8784_yellow_ww Update alert message for mass update request

--- RequestCMR/WebContent/resources/js/massrequestentry/massrequestentry.js
+++ RequestCMR/WebContent/resources/js/massrequestentry/massrequestentry.js
@@ -65,7 +65,7 @@ function processRequestAction() {
   // prevent from overwriting the DB REQ_STATUS
   // if another tab is open with different UI REQ_STATUS
   if (!isReqStatusEqualBetweenUIandDB()) {
-    cmr.showAlert("Unable to save the request. Request Status mismatch from database." +
+    cmr.showAlert("Unable to execute the action. Request Status mismatch from database." +
       "<br><br>Please reload the page.");
 
     return;
