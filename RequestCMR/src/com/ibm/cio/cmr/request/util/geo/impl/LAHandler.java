409843cc2d01c54a3a590413fafd989f03d4609b
Joyce Ann Barcelona <barcelj@ph.ibm.com>
2023-06-02 11:14:35 +0800

    CREATCMR-9535: LA EMAIL

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/LAHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/LAHandler.java
@@ -975,22 +975,10 @@ public class LAHandler extends GEOHandler {
     List<GeoContactInfoModel> tempList = new ArrayList<GeoContactInfoModel>();
     GeoContactInfoModel tempMod = null;
     int loopCount = 0;
-    if (isBRIssuingCountry(modToCopy.getCmrIssuingCntry())) {
-      while (loopCount < 3) {
-        tempMod = new GeoContactInfoModel();
-        copyContactModelProps(modToCopy, tempMod, loopCount);
-        tempList.add(tempMod);
-        loopCount++;
-      }
-    } else if (SystemLocation.PERU.equalsIgnoreCase(modToCopy.getCmrIssuingCntry())) {
-      tempMod = new GeoContactInfoModel();
-      copyContactModelProps(modToCopy, tempMod, loopCount);
-      tempList.add(tempMod);
-    } else if (isMXIssuingCountry(modToCopy.getCmrIssuingCntry())) {
-      tempMod = new GeoContactInfoModel();
-      copyContactModelProps(modToCopy, tempMod, loopCount);
-      tempList.add(tempMod);
-    }
+    tempMod = new GeoContactInfoModel();
+    copyContactModelProps(modToCopy, tempMod, loopCount);
+    tempList.add(tempMod);
+
     return tempList;
   }
 
