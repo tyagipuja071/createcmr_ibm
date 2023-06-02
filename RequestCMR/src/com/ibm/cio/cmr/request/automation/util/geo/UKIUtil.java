d9926e60b58a6394fe382d5ca886ec361adeefc8
Tarishi Geetey <Tarishi.geetey@ibm.com>
2023-06-02 15:21:10 +0530

    CREATCMR-9562 UK coverage Handling

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/UKIUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/UKIUtil.java
@@ -616,23 +616,35 @@ public class UKIUtil extends AutomationUtil {
         }
       }
     }
-
-    List<String> isicList = Arrays.asList("7230", "7240", "7290", "7210", "7221", "7229", "7250", "7123", "9802");
-    if (!(SCENARIO_INTERNAL.equals(scenario) || SCENARIO_PRIVATE_PERSON.equals(scenario) || SCENARIO_BUSINESS_PARTNER.equals(scenario))) {
-      if ("32".equals(data.getIsuCd()) && "S".equals(data.getClientTier()) && StringUtils.isNotBlank(isicCd) && isicList.contains(isicCd)) {
-        details.append("Setting ISU-CTC to '32N' for ISIC: " + isicCd).append("\n");
-        overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE, "DATA", "ISU_CD", data.getIsuCd(), "32");
-        overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE, "DATA", "CLIENT_TIER", data.getClientTier(), "N");
-        results.setProcessOutput(overrides);
-        results.setResults("Calculated.");
-      } else if ("32".equals(data.getIsuCd()) && "N".equals(data.getClientTier()) && StringUtils.isNotBlank(isicCd) && !isicList.contains(isicCd)) {
-        details.append("Setting ISU-CTC to '32S' for ISIC: " + isicCd).append("\n");
-        overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE, "DATA", "ISU_CD", data.getIsuCd(), "32");
-        overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE, "DATA", "CLIENT_TIER", data.getClientTier(), "S");
-        results.setProcessOutput(overrides);
-        results.setResults("Calculated.");
-      }
-    }
+    
+    // List<String> isicList = Arrays.asList("7230", "7240", "7290", "7210",
+    // "7221", "7229", "7250", "7123", "9802");
+    // if (!(SCENARIO_INTERNAL.equals(scenario) ||
+    // SCENARIO_PRIVATE_PERSON.equals(scenario) ||
+    // SCENARIO_BUSINESS_PARTNER.equals(scenario))) {
+    // if ("32".equals(data.getIsuCd()) && "S".equals(data.getClientTier()) &&
+    // StringUtils.isNotBlank(isicCd) && isicList.contains(isicCd)) {
+    // details.append("Setting ISU-CTC to '32N' for ISIC: " +
+    // isicCd).append("\n");
+    // overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE,
+    // "DATA", "ISU_CD", data.getIsuCd(), "32");
+    // overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE,
+    // "DATA", "CLIENT_TIER", data.getClientTier(), "N");
+    // results.setProcessOutput(overrides);
+    // results.setResults("Calculated.");
+    // } else if ("32".equals(data.getIsuCd()) &&
+    // "N".equals(data.getClientTier()) && StringUtils.isNotBlank(isicCd) &&
+    // !isicList.contains(isicCd)) {
+    // details.append("Setting ISU-CTC to '32S' for ISIC: " +
+    // isicCd).append("\n");
+    // overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE,
+    // "DATA", "ISU_CD", data.getIsuCd(), "32");
+    // overrides.addOverride(AutomationElementRegistry.GBL_FIELD_COMPUTE,
+    // "DATA", "CLIENT_TIER", data.getClientTier(), "S");
+    // results.setProcessOutput(overrides);
+    // results.setResults("Calculated.");
+    // }
+    // }
 
     if (details.toString().length() == 0) {
       details.append("No specific fields to calculate.");
@@ -749,12 +761,10 @@ public class UKIUtil extends AutomationUtil {
         }
         details.append("Sales Rep : " + fields.getSalesRep()).append("\n");
         details.append("SBO : " + fields.getSbo()).append("\n");
-        details.append("ISU : " + fields.getIsu()).append("\n");
-        details.append("Client Tier : " + fields.getCtc()).append("\n");
         overrides.addOverride(AutomationElementRegistry.GBL_CALC_COV, "DATA", "SALES_BO_CD", data.getSalesBusOffCd(), fields.getSbo());
         overrides.addOverride(AutomationElementRegistry.GBL_CALC_COV, "DATA", "REP_TEAM_MEMBER_NO", data.getRepTeamMemberNo(), fields.getSalesRep());
-        details.append("ISU : " + fields.getIsu()).append("\n");
-        details.append("Client Tier : " + fields.getCtc()).append("\n");
+        details.append("ISU : " + (StringUtils.isNotBlank(fields.getIsu()) ? fields.getIsu() : data.getIsuCd())).append("\n");
+        details.append("Client Tier : " + (StringUtils.isNotBlank(fields.getCtc()) ? fields.getCtc() : data.getClientTier())).append("\n");
         overrides.addOverride(AutomationElementRegistry.GBL_CALC_COV, "DATA", "ISU_CD", data.getIsuCd(),
             !StringUtils.isBlank(fields.getIsu()) ? fields.getIsu() : data.getIsuCd());
         overrides.addOverride(AutomationElementRegistry.GBL_CALC_COV, "DATA", "CLIENT_TIER", data.getClientTier(),
