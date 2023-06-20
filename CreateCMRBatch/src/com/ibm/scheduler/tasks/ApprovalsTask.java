8b9e46dfd109c4818dc83ab7849b7f92cdc6c1f4
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2023-06-20 20:57:54 -0300

    CREATCMR-9788_Cirrus_Batch_app
    (new) added BatchAppScheduler which is the starting point for running
    the batch scripts from inside java code.

---
+++ CreateCMRBatch/src/com/ibm/scheduler/tasks/ApprovalsTask.java
@@ -0,0 +1,40 @@
+package com.ibm.scheduler.tasks;
+
+import java.io.IOException;
+
+import org.springframework.scheduling.support.CronTrigger;
+
+import com.ibm.scheduler.creator.BatchTask;
+
+public class ApprovalsTask extends BatchTask {
+  String cronExpression = "2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58 * * * *";
+
+  @Override
+  public void run() {
+    System.out.println("Starting " + this.getClass().getCanonicalName() + " ...");
+    Process process;
+    try {
+      process = Runtime.getRuntime().exec("/bin/sh -c /cmr/batch/run_approval_batch.ksh >> /cmr/batch/batch-run.log 2> /cmr/batch/batch-err.log");
+      process.waitFor();
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  @Override
+  public String cronExpression() {
+    return cronExpression;
+  }
+
+  @Override
+  public CronTrigger getCronTrigger() {
+    return new CronTrigger(cronExpression);
+  }
+
+  @Override
+  public boolean isCronEnable() {
+    // TODO fetch this from somewhere
+    return true;
+  }
+}
---
+++ CreateCMRBatch/src/com/ibm/scheduler/tasks/MQServiceSofPubTask.java
@@ -0,0 +1,40 @@
+package com.ibm.scheduler.tasks;
+
+import java.io.IOException;
+
+import org.springframework.scheduling.support.CronTrigger;
+
+import com.ibm.scheduler.creator.BatchTask;
+
+public class MQServiceSofPubTask extends BatchTask {
+  String cronExpression = "01,03,05,07,09,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59 * * * *";
+
+  @Override
+  public void run() {
+    System.out.println("Starting " + this.getClass().getSimpleName() + "...");
+    Process process;
+    try {
+      process = Runtime.getRuntime().exec("/bin/sh -c /cmr/batch/run_sofpub.ksh >> /cmr/batch/batch-run.log 2> /cmr/batch/batch-err.log");
+      process.waitFor();
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  @Override
+  public String cronExpression() {
+    return cronExpression;
+  }
+
+  @Override
+  public CronTrigger getCronTrigger() {
+    return new CronTrigger(cronExpression);
+  }
+
+  @Override
+  public boolean isCronEnable() {
+    // TODO fetch this from somewhere
+    return true;
+  }
+}
