61d7a9b9e6e6d4d620c900404a49b86a7f36f326
Jojo Durante <durantjp@ph.ibm.com>
2023-06-20 20:43:05 +0800

    fix nullpointer exception

--- RequestCMR/src/com/ibm/cio/cmr/request/service/requestentry/RequestEntryService.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/service/requestentry/RequestEntryService.java
@@ -648,18 +648,23 @@ public class RequestEntryService extends BaseService<RequestEntryModel, Compound
         } else
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_NA);
       }
+      
+      if (oldVatIndValue == null){
+        oldVatIndValue = "";
+      } 
+      
       if (admin.getReqType().equals("U")) {
-        if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue != null ) && oldVatIndValue.equals('N')) {          
+        if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue != null ) && oldVatIndValue.equals("N")) {          
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_YES);
-        } else if ("N".equals(data.getVatInd()) && (iscrossBorder) && (oldVatValue != null) && oldVatIndValue.equals('N')) {          
+        } else if ("N".equals(data.getVatInd()) && (iscrossBorder) && (oldVatValue != null) && oldVatIndValue.equals("N")) {          
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_NA);
-        } else if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue == null || oldVatValue.isEmpty()) && oldVatIndValue.equals('N')) {
+        } else if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue == null || oldVatValue.isEmpty()) && oldVatIndValue.equals("N")) {
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_NA);
-        } else if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue != null) && data.getVat().isEmpty() && (oldVatIndValue.equals('T'))) {
+        } else if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue != null) && data.getVat().isEmpty() && (oldVatIndValue.equals("T"))) {
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_YES);        
-        } else if ("N".equals(data.getVatInd()) && (iscrossBorder) && (oldVatValue != null) && data.getVat().isEmpty() && (oldVatIndValue.equals('E'))) {
+        } else if ("N".equals(data.getVatInd()) && (iscrossBorder) && (oldVatValue != null) && data.getVat().isEmpty() && (oldVatIndValue.equals("E"))) {
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_YES);
-        } else if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue == null|| oldVatValue.isEmpty()) && data.getVat().isEmpty() && (oldVatIndValue.equals('E'))) {
+        } else if ("N".equals(data.getVatInd()) && (!iscrossBorder) && (oldVatValue == null|| oldVatValue.isEmpty()) && data.getVat().isEmpty() && (oldVatIndValue.equals("E"))) {
           scorecard.setVatAcknowledge(CmrConstants.VAT_ACKNOWLEDGE_YES);
         }
       }
