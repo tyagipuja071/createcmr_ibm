ffa627a8c6735a76d3e90a07fef632e54a9faf2d
Joyce Ann Barcelona <barcelj@ph.ibm.com>
2023-08-01 22:45:15 +0800

    CREATCMR-9551: LA FIELDS ADD REMOVE

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/LAHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/LAHandler.java
@@ -364,10 +364,6 @@ public class LAHandler extends GEOHandler {
       if (laReactivateCapable)
         data.setFunc("R");
     }
-
-    if (CmrConstants.REQ_TYPE_UPDATE.equals(admin.getReqType())) {
-      data.setRepTeamMemberNo(DEFAULT_SALES_REP);
-    }
   }
 
   private void importTaxInfo(EntityManager entityManager, Data data, long reqId, String sapNumber, String requesterId) {
@@ -793,7 +789,6 @@ public class LAHandler extends GEOHandler {
       data.setSalesRepTeamDateOfAssignment(SystemUtil.getCurrentTimestamp());
 
       data.setInstallRepTeamDateOfAssignment(SystemUtil.getCurrentTimestamp());
-      data.setInstallRep(data.getRepTeamMemberNo());
       data.setPaymentAddrCd("N");
 
     }
@@ -1598,24 +1593,6 @@ public class LAHandler extends GEOHandler {
       update.setDataField(PageManager.getLabel(cntry, "BGLDERule", "-"));
       update.setNewData(service.getCodeAndDescription(newData.getBgRuleId(), "BGLDERule", cmrCountry));
       update.setOldData(service.getCodeAndDescription(oldData.getBgRuleId(), "BGLDERule", cmrCountry));
-      results.add(update);
-    }
-
-    if (RequestSummaryService.TYPE_IBM.equals(type) && !equals(oldData.getRepresentativeTeamMemberNo(), newData.getRepTeamMemberNo())) {
-      update = new UpdatedDataModel();
-      String cntry = null;
-      update.setDataField(PageManager.getLabel(cntry, "SalRepNameNo", "-"));
-      update.setNewData(service.getCodeAndDescription(newData.getRepTeamMemberNo(), "SalRepNameNo", cmrCountry));
-      update.setOldData(service.getCodeAndDescription(oldData.getRepresentativeTeamMemberNo(), "SalRepNameNo", cmrCountry));
-      results.add(update);
-    }
-
-    if (RequestSummaryService.TYPE_IBM.equals(type) && !equals(oldData.getMarketingChnlIndcValue(), newData.getMrktChannelInd())) {
-      update = new UpdatedDataModel();
-      String cntry = null;
-      update.setDataField(PageManager.getLabel(cntry, "MrktChannelInd", "-"));
-      update.setNewData(service.getCodeAndDescription(newData.getMrktChannelInd(), "MrktChannelInd", cmrCountry));
-      update.setOldData(service.getCodeAndDescription(oldData.getMarketingChnlIndcValue(), "MrktChannelInd", cmrCountry));
       results.add(update);
     }
 
@@ -3353,7 +3330,6 @@ public class LAHandler extends GEOHandler {
     map.put("##RequestReason", "reqReason");
     map.put("##CustomerType", "custType");
     map.put("##POBox", "poBox");
-    map.put("##MrktChannelInd", "mrktChannelInd");
     map.put("##LandedCountry", "landCntry");
     map.put("##CMRIssuingCountry", "cmrIssuingCntry");
     map.put("##INACCode", "inacCd");
@@ -3380,7 +3356,6 @@ public class LAHandler extends GEOHandler {
     map.put("##BGLDERule", "bgRuleId");
     map.put("##ProspectToLegalCMR", "prospLegalInd");
     map.put("##ClientTier", "clientTier");
-    map.put("##SalRepNameNo", "repTeamMemberNo");
     map.put("##ProxiLocationNumber", "proxiLocnNo");
     map.put("##DropDownCity", "city1");
     map.put("##SAPNumber", "sapNo");
