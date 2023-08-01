8cb9ec244c21e363d931319bc10512b6c19db076
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2023-08-01 02:56:30 -0300

    CREATCMR-9788_CirrusMove_Batch

--- CreateCMRBatch/src/com/ibm/scheduler/interfaces/ProcessStarter.java
+++ CreateCMRBatch/src/com/ibm/scheduler/interfaces/ProcessStarter.java
@@ -3,18 +3,16 @@ package com.ibm.scheduler.interfaces;
 import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
 
-import org.apache.logging.log4j.ThreadContext;
-
 public interface ProcessStarter {
 
   public default void runProcess(String command, String processName) {
-    ThreadContext.put("PROCESS_NAME", processName);
     try {
       ProcessBuilder pb = new ProcessBuilder(command.split(" "));
       pb.redirectOutput(Redirect.INHERIT);
       pb.redirectError(Redirect.INHERIT);
       pb.start();
-    } catch (IOException e) {
+      pb.wait();
+    } catch (IOException | InterruptedException e) {
       e.printStackTrace();
     }
   }
