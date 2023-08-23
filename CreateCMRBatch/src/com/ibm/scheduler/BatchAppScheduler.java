e40fb117ba3e9d07ce32827a59cd0a09a2204e6d
Hesller Huller Gomes da Silva <Hesller.Huller.Gomes.Da.Silva@ibm.com>
2023-08-23 10:06:10 -0300

    CREATCMR-9788_CirrusMove
    (fix) add missing class scheduler.

---
+++ CreateCMRBatch/src/com/ibm/scheduler/BatchAppScheduler.java
@@ -0,0 +1,20 @@
+package com.ibm.scheduler;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import com.ibm.scheduler.creator.BatchTask;
+import com.ibm.scheduler.creator.BatchTaskCreator;
+
+public class BatchAppScheduler {
+
+  public static void main(String[] args) {
+    ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+    scheduler.setPoolSize(BatchTaskCreator.getTotalNumberOfBatches());
+    scheduler.initialize();
+    for (BatchTask cronTask : BatchTaskCreator.createTasks()) {
+      if (cronTask.isCronEnable()) {
+        scheduler.schedule(cronTask, cronTask.getCronTrigger());
+      }
+    }
+  }
+}
