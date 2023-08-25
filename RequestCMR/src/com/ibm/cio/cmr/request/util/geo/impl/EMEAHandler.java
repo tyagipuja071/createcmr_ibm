ebba0faa0ddeb8bf5902a8a068c5d24f8fc0394e
vinod-kumar-saharma <117459460+vinod-kumar-saharma@users.noreply.github.com>
2023-08-25 17:42:24 +0530

    CREATCMR-9758_UKI_Prospect import Nam3 and Nam4

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/EMEAHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/EMEAHandler.java
@@ -154,13 +154,7 @@ public class EMEAHandler extends BaseSOFHandler {
     if (CmrConstants.REQ_TYPE_CREATE.equals(reqEntry.getReqType())) {
       // only add zs01 equivalent for create by model
       FindCMRRecordModel record = mainRecord;
-      if (SystemLocation.UNITED_KINGDOM.equals(record.getCmrIssuedBy()) || SystemLocation.IRELAND.equals(record.getCmrIssuedBy())) {
-        record.setCmrName4(record.getCmrName4());
-      } else {
-        // name4 in rdc = Attn on SOF
-        record.setCmrDept(record.getCmrName4());
-        record.setCmrName4(null);
-      }
+
       if (SystemLocation.UNITED_KINGDOM.equals(record.getCmrIssuedBy()) || SystemLocation.IRELAND.equals(record.getCmrIssuedBy())) {
         record.setCmrStreetAddressCont(record.getCmrName4());
         record.setCmrName3(record.getCmrName3());
@@ -169,7 +163,11 @@ public class EMEAHandler extends BaseSOFHandler {
         record.setCmrStreetAddressCont(record.getCmrName3());
         record.setCmrName3(null);
       }
-
+      // name4 in rdc = Attn on SOF
+      if (!("866".equals(SystemLocation.UNITED_KINGDOM) || ("754".equals(SystemLocation.IRELAND)))) {
+        record.setCmrDept(record.getCmrName4());
+        record.setCmrName4(null);
+      }
       if (!StringUtils.isBlank(record.getCmrPOBox())) {
         if (SystemLocation.UNITED_KINGDOM.equals(record.getCmrIssuedBy()) || SystemLocation.IRELAND.equals(record.getCmrIssuedBy())) {
           record.setCmrPOBox(record.getCmrPOBox());
