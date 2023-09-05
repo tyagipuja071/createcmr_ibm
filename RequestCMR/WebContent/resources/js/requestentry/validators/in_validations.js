ce7e4b433b4c1f09aecec3097c6e7c58d4395456
Aarthi <Aarthi.V@ibm.com>
2023-09-05 13:37:14 +0530

    CREATCMR-10216_Inisbu

--- RequestCMR/WebContent/resources/js/requestentry/validators/in_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/in_validations.js
@@ -38,6 +38,7 @@ function custSubGrpHandler() {
   if (_custSubGrpHandler == null) {
     _custSubGrpHandler = dojo.connect(FormManager.getField('custSubGrp'), 'onChange', function(value) {
       onIsicChange();
+      prospectFilterISBU();
     });
   }
 }
@@ -83,6 +84,16 @@ function resetGstExempt() {
     console.log(">>> Process gstExempt remove * >> ");
     FormManager.resetValidations('vat');
     FormManager.readOnly('vat');
+  }
+}
+
+function prospectFilterISBU() {
+  var ifProspect = FormManager.getActualValue('prospLegalInd');
+  if (dijit.byId('prospLegalInd')) {
+    ifProspect = dijit.byId('prospLegalInd').get('checked') ? 'Y' : 'N';
+  }
+  if (ifProspect == 'Y') {
+    setISBUScenarioLogic();
   }
 }
 
