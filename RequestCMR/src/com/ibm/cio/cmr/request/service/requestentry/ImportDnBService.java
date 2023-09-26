616f46f6ea9d419ad11cfdc0de0cdb8a10f39041
Rochelle Salazar <salazarm@ph.ibm.com>
2023-09-26 20:46:05 +0800

    fix for build issue

--- RequestCMR/src/com/ibm/cio/cmr/request/service/requestentry/ImportDnBService.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/service/requestentry/ImportDnBService.java
@@ -6,6 +6,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
