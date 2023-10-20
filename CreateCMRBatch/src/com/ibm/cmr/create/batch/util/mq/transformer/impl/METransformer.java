f9050dda4c310661aeaa14adf74006a39cfe799e
Ayushi . <AYUSHI.Ayushi@ibm.com>
2023-10-20 12:53:46 +0530

    CREATCMR 10209 STC MassUpdt

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/METransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/METransformer.java
@@ -1465,7 +1465,7 @@ public class METransformer extends EMEATransformer {
       cust.setAbbrevLocn(muData.getAbbrevLocn());
     }
 
-    if (!StringUtils.isBlank(muData.getMiscBillCd())) {
+    if (!StringUtils.isBlank(muData.getMiscBillCd()) || "ST".equalsIgnoreCase(muData.getTaxExemptStatus3())) {
       if ("@".equals(muData.getMiscBillCd()) || "ST".equalsIgnoreCase(muData.getTaxExemptStatus3())) {
         cust.setEmbargoCd("");
       } else {
--- CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/SpainTransformer.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/util/mq/transformer/impl/SpainTransformer.java
@@ -796,7 +796,7 @@ public class SpainTransformer extends MessageTransformer {
       }
     }
 
-    if (!StringUtils.isBlank(muData.getMiscBillCd())) {
+    if (!StringUtils.isBlank(muData.getMiscBillCd()) || "ST".equalsIgnoreCase(muData.getTaxExemptStatus3())) {
       if ("@".equals(muData.getMiscBillCd().trim()) || "ST".equalsIgnoreCase(muData.getTaxExemptStatus3())) {
         cust.setEmbargoCd("");
       } else {
