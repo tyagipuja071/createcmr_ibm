02a7c3ea5a5952f48fe3d5573783928fed9fdc32
ZHI GUANG SUN <sunzg@cn.ibm.com>
2023-11-14 20:02:56 +0800

    CREATCMR-10081

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/ANZHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/ANZHandler.java
@@ -438,8 +438,10 @@ public class ANZHandler extends GEOHandler {
   
   @Override
   public void setDataValuesOnImport(Admin admin, Data data, FindCMRResultModel results, FindCMRRecordModel mainRecord) throws Exception {
-    // TODO Auto-generated method stub
-    
+    if (CmrConstants.REQ_TYPE_UPDATE.equals(admin.getReqType()) && "796".equals(data.getCmrIssuingCntry())) {
+      admin.setOldCustNm1(mainRecord.getCmrName1Plain());
+      admin.setOldCustNm2(mainRecord.getCmrName2Plain());
+    }
   }
 
   @Override
