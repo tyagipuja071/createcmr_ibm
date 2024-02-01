d1680947c3fe38a75e77f71589ae6b50f25b32cf
Vaishali Varshney <vaishali.varshney@ibm.com>
2024-02-01 14:36:25 +0530

    FIX FOR CREATCMR-10935

--- RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/AustraliaUtil.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/automation/util/geo/AustraliaUtil.java
@@ -2,7 +2,6 @@ package com.ibm.cio.cmr.request.automation.util.geo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -592,12 +591,14 @@ public class AustraliaUtil extends AutomationUtil {
     GEOHandler handler = RequestUtils.getGEOHandler(data.getCmrIssuingCntry());
 
     List<Addr> addresses = null;
+    Addr soldTo = requestData.getAddress("ZS01");
     StringBuilder checkDetails = new StringBuilder();
     Set<String> resultCodes = new HashSet<String>();// R - review
     for (String addrType : RELEVANT_ADDRESSES) {
       if (changes.isAddressChanged(addrType)) {
         if (CmrConstants.RDC_SOLD_TO.equals(addrType)) {
-          addresses = Collections.singletonList(requestData.getAddress(CmrConstants.RDC_SOLD_TO));
+          addresses = requestData.getAddresses();
+         
         } else {
           addresses = requestData.getAddresses(addrType);
         }
