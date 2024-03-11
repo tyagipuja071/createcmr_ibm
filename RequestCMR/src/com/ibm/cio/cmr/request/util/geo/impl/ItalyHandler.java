9474d96241fd1b4ae3504143a3332adcee1eb3b2
Ayushi . <AYUSHI.Ayushi@ibm.com>
2024-03-11 17:43:59 +0530

    CREATCMR 10730 IT COV

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/ItalyHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/ItalyHandler.java
@@ -1918,13 +1918,18 @@ public class ItalyHandler extends BaseSOFHandler {
               error.addError((row.getRowNum() + 1), "Client Tier",
                   ":Note that Client Tier should be 'Q' for the selected ISU code. Please fix and upload the template again.<br>");
             }
-          } else if (!StringUtils.isBlank(isu) && "32".equals(isu)) {
-            if (!"T".contains(clientTier) || StringUtils.isBlank(clientTier)) {
+          } else if (!StringUtils.isBlank(isu) && "27".equals(isu)) {
+            if (!"E".contains(clientTier) || StringUtils.isBlank(clientTier)) {
               LOG.trace("The row " + (row.getRowNum() + 1)
-                  + ":Note that Client Tier should be 'T' for the selected ISU code. Please fix and upload the template again.");
+                  + ":Note that Client Tier should be 'E' for the selected ISU code. Please fix and upload the template again.");
               error.addError((row.getRowNum() + 1), "Client Tier",
-                  ":Note that Client Tier should be 'T' for the selected ISU code. Please fix and upload the template again.<br>");
+                  ":Note that Client Tier should be 'E' for the selected ISU code. Please fix and upload the template again.<br>");
             }
+          } else if ((!StringUtils.isBlank(isu) && isu.startsWith("32")) || (StringUtils.isBlank(clientTier) || "T".contains(clientTier))) {
+            LOG.trace(
+                "The row " + (rowIndex + 1) + ":Note that ISU 32 & Client Tier T has been obsolete.. Please fix and upload the template again.");
+            error.addError((rowIndex + 1), "Client Tier",
+                ":Note that ISU 32 & Client Tier T has been obsolete. Please fix and upload the template again.<br>");
           } else if (!StringUtils.isBlank(isu) && "36".equals(isu)) {
             if (!"Y".contains(clientTier) || StringUtils.isBlank(clientTier)) {
               LOG.trace("The row " + (row.getRowNum() + 1)
@@ -1937,9 +1942,9 @@ public class ItalyHandler extends BaseSOFHandler {
             error.addError(row.getRowNum() + 1, "Client Tier", "Client Tier Value should always be @ for IsuCd Value :" + isu + ".<br>");
           } else if (!"@QYT".contains(clientTier)) {
             LOG.trace(
-                "The row " + (row.getRowNum() + 1) + ":Note that Client Tier only accept @,Q,Y or T. Please fix and upload the template again.");
+                "The row " + (row.getRowNum() + 1) + ":Note that Client Tier only accept @,Q,Y or E. Please fix and upload the template again.");
             error.addError((row.getRowNum() + 1), "Client Tier",
-                ":Note that Client Tier only accept @,Q,Y or T. Please fix and upload the template again.<br>");
+                ":Note that Client Tier only accept @,Q,Y or E. Please fix and upload the template again.<br>");
           }
           if (StringUtils.isNotBlank(stcOrdBlk) && StringUtils.isNotBlank(ordBlk)) {
             LOG.trace("Please fill either STC Order Block Code or Embargo Code ");
