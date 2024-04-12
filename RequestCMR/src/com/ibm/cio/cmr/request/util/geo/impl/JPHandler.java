f18da3a7b61170fc1dee22feefc8077c8fe30061
Seff Malonda <joseph.malonda@ibm.com>
2024-04-12 19:51:15 +0800

    CREATCMR-11763_yellow_jp place trim before substring

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/JPHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/JPHandler.java
@@ -1044,8 +1044,8 @@ public class JPHandler extends GEOHandler {
     data.setTerritoryCd(mainRecord.getCmrPOBoxPostCode());
 
     data.setSvcArOffice(mainRecord.getCmrCustGrpId());
-    data.setAgreementSignDate(mainRecord.getCmrContractSignDt() != null && mainRecord.getCmrContractSignDt().length() == 8
-        ? mainRecord.getCmrContractSignDt().trim().substring(2) : mainRecord.getCmrContractSignDt().trim());
+    data.setAgreementSignDate(mainRecord.getCmrContractSignDt() != null && mainRecord.getCmrContractSignDt().trim().length() == 8
+        ? mainRecord.getCmrContractSignDt().substring(2) : mainRecord.getCmrContractSignDt().trim());
 
     if (mainRecord.getCompanyCd() != null) {
       if (mainRecord.getCompanyCd().equals("AA")) {
