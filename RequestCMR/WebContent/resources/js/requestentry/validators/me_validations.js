8d1b0f2e8968d1227990603abd05097be0b56a99
Jojo Durante <durantjp@ph.ibm.com>
2024-04-25 23:41:15 +0800

    All address types are mandatory issue

--- RequestCMR/WebContent/resources/js/requestentry/validators/me_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/me_validations.js
@@ -862,7 +862,7 @@ function addAddressTypeValidatorME() {
             }
           }
 
-          if (reqLocalAddr.has(cntry) && (zs01Cnt == 0 || zp01Cnt == 0 || zi01Cnt == 0 || zd01Cnt == 0 || zs02Cnt == 0 || zp02Cnt == 0)) {
+          if (reqLocalAddr.has(cntry) && (zs01Cnt == 0 || zp01Cnt == 0 || zi01Cnt == 0 || zd01Cnt == 0 || zs02Cnt == 0)) {
             return new ValidationResult(null, false, 'All address types are mandatory.');
           } else if (zs01Cnt == 0 || zp01Cnt == 0 || zi01Cnt == 0 || zd01Cnt == 0 || zs02Cnt == 0) {
             return new ValidationResult(null, false, 'All address types are mandatory except G Address.');
