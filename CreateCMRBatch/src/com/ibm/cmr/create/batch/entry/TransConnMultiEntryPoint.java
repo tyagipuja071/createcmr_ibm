27ef9122bb95d5fbc84f1d10377d5717a706db88
Bikash Das <Bikash.Das1@ibm.com>
2024-05-20 12:14:40 +0530

    CREATCMR-11939 - TransConn removed from main.

--- CreateCMRBatch/src/com/ibm/cmr/create/batch/entry/TransConnMultiEntryPoint.java
+++ CreateCMRBatch/src/com/ibm/cmr/create/batch/entry/TransConnMultiEntryPoint.java
@@ -33,9 +33,6 @@ public class TransConnMultiEntryPoint extends BatchEntryPoint {
     service.setMode(Mode.Aborted);
     service.execute();
 
-    service.setMode(Mode.Pending);
-    service.execute();
-
     service.setMode(Mode.MQ);
     service.execute();
 
