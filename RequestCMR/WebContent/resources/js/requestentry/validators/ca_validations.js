bf1412b4c8a0608b1767b2ec221d6d4b51d842be
Ayushi . <AYUSHI.Ayushi@ibm.com>
2024-10-28 11:45:56 +0530

    CREATCMR - 10819 Cust Class

--- RequestCMR/WebContent/resources/js/requestentry/validators/ca_validations.js
+++ RequestCMR/WebContent/resources/js/requestentry/validators/ca_validations.js
@@ -1547,7 +1547,9 @@ dojo.addOnLoad(function() {
   GEOHandler.addAfterTemplateLoad(preTickLatePaymentInd, SysLoc.CANADA);
   GEOHandler.addAfterTemplateLoad(limitDropdownOnScenarioChange, SysLoc.CANADA);
   GEOHandler.addAfterTemplateLoad(setCSBranchValue, SysLoc.CANADA);
+ 
   GEOHandler.addAfterTemplateLoad(setDefaultARFAARByScenario, SysLoc.CANADA);
+  GEOHandler.addAfterTemplateLoad(setCustClass, SysLoc.CANADA);
   GEOHandler.addAfterConfig(enableCustClass, SysLoc.CANADA);
   GEOHandler.addToggleAddrTypeFunction(hideObsoleteAddressOption, [ SysLoc.CANADA ]);
   GEOHandler.addAddrFunction(addStateProvHandler, [ SysLoc.CANADA ]);
