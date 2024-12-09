66299a84172fe0457630960e9c472fdfe1ce62b3
Joyce Ann Barcelona <barcelj@ph.ibm.com>
2024-12-09 21:31:15 +0800

    CREATCMR-12683: ASEAN AR CODE

--- RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/ASEANHandler.java
+++ RequestCMR/src/com/ibm/cio/cmr/request/util/geo/impl/ASEANHandler.java
@@ -15,7 +15,11 @@ import javax.persistence.EntityManager;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.ibm.cio.cmr.request.CmrConstants;
+import com.ibm.cio.cmr.request.entity.Admin;
+import com.ibm.cio.cmr.request.entity.Data;
 import com.ibm.cio.cmr.request.model.requestentry.FindCMRRecordModel;
+import com.ibm.cio.cmr.request.model.requestentry.FindCMRResultModel;
 import com.ibm.cio.cmr.request.util.SystemLocation;
 import com.ibm.cio.cmr.request.util.geo.GEOHandler;
 import com.ibm.cio.cmr.request.util.wtaas.WtaasAddress;
@@ -735,6 +739,15 @@ public class ASEANHandler extends APHandler {
     map.put("##CustomerScenarioSubType", "custSubGrp");
     map.put("##RegionCode", "miscBillCd");
     return map;
+  }
+
+  @Override
+  public void setDataValuesOnImport(Admin admin, Data data, FindCMRResultModel results, FindCMRRecordModel mainRecord) throws Exception {
+    super.setDataValuesOnImport(admin, data, results, mainRecord);
+    List<String> aseanCountries = Arrays.asList(SystemLocation.THAILAND, SystemLocation.MALAYSIA, SystemLocation.INDONESIA, SystemLocation.SINGAPORE);
+    if (CmrConstants.REQ_TYPE_UPDATE.equals(admin.getReqType()) && aseanCountries.contains(data.getCmrIssuingCntry())) {
+      data.setCollectionCd(mainRecord.getCmrAccRecvBo() != null ? mainRecord.getCmrAccRecvBo() : "0000");
+    }
   }
 
   @Override
